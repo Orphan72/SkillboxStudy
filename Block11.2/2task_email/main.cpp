ff5acd307bee0aa2122738be1405652f2e00595b
Orphan72 <sdenko17@gmail.com>
2021-06-07 20:49:20 +0300

    finish

--- Block11.2/2task_email/main.cpp
+++ Block11.2/2task_email/main.cpp
@@ -1,119 +1,115 @@
 #include <iostream>
 
-//функция должна быть булевая, значит внутри должен быть флаг
-
-bool fun (std::string str)
+std::string firstPart (std::string str)
 {
     bool et = false;
-    bool correct = true;
     int etPos = 0;
-    int startSecond = 0;
-
+    std::string strResult = "";
     for (int i = 0; !et && i < str.length(); i++)
     {
-        //std::cout << str[i];
         if (str [i] == '@')
-       {
-           et = true;
-           etPos = i;
-           startSecond = i + 1;
-           //std::cout << str[i] << std::endl;
-           //std::cout << i << std::endl;
-       }
+        {
+            et = true;
+            etPos = i;
+        }
     }
 
-    if (!et) return false;
-    else
+    if (et)
     {
         for (int i = 0; i < etPos; i++)
         {
-            std::cout << str[i];
-            if (str [0] == '.') return false;
-            else if (str[i] == '.' && str[i - 1] == '.') return false;
-
+            strResult += str[i];
         }
-        std::cout << std::endl;
-        for (int i = startSecond; i < str.length(); i++)
-        {
-
-            int endPos = str.length() - 1;
-            if (str [endPos] == '.') return false;
-            else if (str[i] == '.' && str[i - 1] == '.') return false;
-            std::cout << str[i];
-        }
-        std::cout << std::endl;
-
     }
 
-    /*
-    int endPos = str.length() - 1;
-    if ((str [0] == '.') || (str[endPos] == '.') ||
-        (str[i] == '.' && str[i - 1] == '.'))
-        return false;
-    */
+    return strResult;
+}
 
-/*
-    for (int i = etPos + 1; i < str.length(); i++)
+std::string secondPart (std::string str)
+{
+    bool et = false;
+    int etPos = 0;
+    std::string strResult = "";
+    for (int i = 0; !et && i < str.length(); i++)
     {
-        if (str [i] == '@') return false;
+        if (str [i] == '@')
+        {
+            et = true;
+            etPos = i + 1;
+        }
     }
 
-*/
-    return correct;
+    if (et)
+    {
+        for (int i = etPos; i < str.length(); i++)
+        {
+            strResult += str[i];
+        }
+    }
+
+    return strResult;
+}
+
+bool checkSymbols (std::string str, std::string symbols)
+{
+    bool sym = false;
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (str[i] == '.' && str[i - 1] == '.') return false;
+
+        sym = ((str [i] >= '0' && str [i] <= '9') ||
+               (str [i] >= 'A'  && str [i] <= 'Z') ||
+               (str [i] >= 'a'  && str [i] <= 'z'));
+
+        if (!sym)
+        {
+            for (int j = 0; j < symbols.length(); j++)
+            {
+                sym = (str[i] == symbols[j]);
+                if (sym) break;
+                if (j == (symbols.length() - 1) && sym == false)
+                    return false;
+            }
+        }
+        //sym = false;
+    }
+    return sym;
+}
+
+bool checkFirst (std::string str)
+{
+    std::string symFirst = ".!#$%&'*+-/=?^_`{|}~";
+    if (str [0] == '.' || str.length() < 1 || str.length() > 64)
+        return false;
+
+    return checkSymbols (str, symFirst);
+
+}
+
+bool checkSecond (std::string str)
+{
+
+    std::string symSec = ".-";
+    int last = str.length() - 1;
+    if (str [last] == '.' || str.length() < 1 || str.length() > 63)
+        return false;
+
+    return checkSymbols (str, symSec);
 }
 
 int main()
 {
     std::string str = "";
 
-    bool et = false;
-    int etPos = 0;
-    int secondStart = 0;
-    bool cor = true;
-
     std::cout << "Enter email\n=>";
     std::getline(std::cin, str);
 
-    cor = fun (str);
+    std::string strFirst = firstPart (str);
+    std::string strSecond = secondPart (str);
 
+    bool correct = (checkFirst(strFirst) && checkSecond(strSecond));
 
-    if (cor)
-    {
-        std::cout << "Yes. " << etPos << "\n";
-    }
-    else
-    {
-        std::cout << "No\n";
-    }
-
-    //std::cout << (correct ? "Yes.\n" : "No\n");
-    //correct ? std::cout << "Yes.\n" : "No\n"
-
-
-
-    /*
-
-    bool cor = false;
-    while (!cor)
-    {
-        std::cout << "Enter email\n=>";
-        std::getline(std::cin, str);
-        cor = fun (str);
-        std::cout << (cor ? "Yes\n" : "No\n");
-    }
-
-*/
+    std::cout << (correct ? "Yes\n" : "No\n");
     return 0;
 }
 
-
-/*
- Согласно международным требованиям, корректный email-адрес должен состоять из двух частей, разделённых знаком @.
- Первая часть должна иметь длину не менее одного и не более 64 символов, вторая часть — не менее одного и не более 63 символов.
- Из символов допускаются только английские буквы, цифры и знак «-» (дефис), а также точка. Точка не может быть первым или последним символом,
- а кроме того, две точки не могут идти подряд. В первой части (которая предшествует символу @), кроме вышеперечисленных, разрешены ещё следующие символы:
-
-!#$%&'*+-/=?^_`{|}~
-Пользователь вводит строку, задающую email-адрес. Программа должна вывести слово Yes, если адрес корректен, а в противном случае — слово No.
-
- */

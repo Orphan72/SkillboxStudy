1a77206157d00ca1b60705dd7278eab6bd51aabb
Orphan72 <sdenko17@gmail.com>
2021-06-22 08:47:51 +0300

    210622Task4

--- Block11.2/4task_compar Long/main.cpp
+++ Block11.2/4task_compar Long/main.cpp
@@ -1,123 +1,312 @@
 #include <iostream>
 
-std::string get_firstPart (std::string str)
+int find_dot (std::string str)
 {
-    bool dot = false;
-    int dotPos = 0;
-    std::string strResult = "";
-    for (int i = 0; !dot && i < str.length(); i++) {
-        if (str [i] == '.') {
-            dot = true;
-            dotPos = i;
-        }
-    }
-
-    if (dot) {
-        for (int i = 0; i < dotPos; i++) {
-            strResult += str[i];
-        }
-    }
-
-    return strResult;
-}
-
-std::string get_secondPart (std::string str) {
-    bool dot = false;
-    int dotPos = 0;
-    std::string strResult = "";
-    for (int i = 0; !dot && i < str.length(); i++) {
-        if (str [i] == '.') {
-            dot = true;
-            dotPos = i + 1;
-        }
-    }
-
-    if (dot) {
-        for (int i = dotPos; i < str.length(); i++) {
-            strResult += str[i];
-        }
-    }
-
-    return strResult;
-}
-
-int check (std::string str, bool minus)
-{
-    int start  = minus ? 1 : 0;
-    std::cout << "start " << start << "\n";
-    int length = str.length();
-    std::cout << "length " << length << "\n";
-    for (int i = 0; i < 3; i++)
+    int dt = -1;
+    for (int i = 0; dt == -1 && i < str.length(); i++)
     {
-        std::cout << i << " " << str [i] << "\n";
+        if (str [i] == '.')
+            dt = i;
+    }
+    return dt;
+}
+
+std::string get_intPart (std::string str) {
+    std::string strResult = "";
+    int dot = find_dot(str);
+    if (dot == -1)
+    {
+        return str;
+    }
+
+    for (int i = 0; i < dot; i++) {
+            strResult += str[i];
+    }
+    return strResult;
+}
+
+std::string get_fracPart (std::string str) {
+    std::string strResult = "";
+    int dot = find_dot(str);
+    if (dot == -1)
+    {
+        return "";
+    }
+    for (int i = dot + 1; i < str.length(); i++) {
+       strResult += str[i];
+    }
+    return strResult;
+}
+
+bool checkSymbol (std::string str)
+{
+    bool minus = (str.length() > 0 && str [0] == '-');
+    int start  = minus ? 1 : 0;
+    //std::cout << "start " << start << "\n";
+    //int length = str.length();
+    //std::cout << "length " << length << "\n";
+    for (int i = start; i < str.length(); i++)
+    {
         if (str [i] < '0' || str [i] > '9')
         {
             //std::cout << "incorr symbols: \n";
-            std::cout << i << " " << str [i] << "\n";
-            //return 10;
+            //std::cout << i << " " << str [i] << "\n";
+            return false;
         }
-        //else return 78;
     }
-
+    return true;
 }
 
+/*
 
-int find_dot (std::string str)
+bool correctEnter (std::string str)
 {
-    int dot = -1;
-    for (int i = 0; dot == -1 && i < str.length(); i++)
-    {
-        if (str [i] == '.')
-        dot = i;
+    //bool cor = false;
+    bool minus = (str.length() > 0 && str [0] == '-');
+    int dot = find_dot(str);
+    if (dot == -1) {
+        return checkSymbol(str, minus);
+        //std::cout << "correct68 - " << correct << "\n";
+        //std::cout << "str - " << str << "\n";
+
     }
-    return dot;
+    else {
+        std::string strIntPart = get_intPart (str);
+        std::cout << "strIntPart: " << strIntPart << "\n";
+        std::string strFracPart = get_fracPart(str);
+        std::cout << "strFracPart: " << strFracPart << "\n";
+        return (checkSymbol(strIntPart, minus) && checkSymbol(strFracPart, false));
+        //std::cout << "correct78 - " << correct << "\n";
+    }
 }
+
+*/
+
+
+
+
+
+int convertInt (std::string str) {
+
+    bool minus = (str.length() > 0 && str [0] == '-');
+    int strInt = 0;
+    int endPos = str.length() - 1;
+    int factor = 1;
+
+    int start  = minus ? 1 : 0;
+    for (int i = endPos; i >= start; i--) {
+        strInt += (factor * (str [i] - '0'));
+        factor *= 10;
+    }
+    strInt  = minus ? -strInt : strInt;
+    return strInt;
+}
+
+
+int convertFrac (std::string str) {
+    int strFrac = 0;
+    int endPos = str.length() - 1;
+    float divider = 1.0;
+    for (int i = endPos;  i >= 0; i--) {
+        //std::cout  << "length [] = " << str.length() << std::endl;
+        //std::cout  << "i = " << i << std::endl;
+
+        strFrac += (divider * (str [i] - '0'));
+        divider *= 10;
+        std::cout  << "divider = " << divider << std::endl;
+    }
+
+    strFrac /= divider;
+    /* for (int i = 0; i < str.length(); i++) {
+          divider *=10;
+     }*/
+
+    //strint  = mns ? -strint : strint;
+    return strFrac;
+}
+
+
+
+// в предыдущем заданиии пподправить метод convert
+float convert (std::string str) {
+
+
+    int strInt_int = 0;
+    float strFrac_float = 0;
+
+    bool minus = (str.length() > 0 && str [0] == '-');
+    int dot = find_dot(str);
+    if (dot == -1)
+    {
+        return convertInt (str);
+
+    }
+    else
+    {
+
+
+        //strInt_int = convertInt (strIntPart);
+        std::cout << "strInt_int: " << strInt_int << "\n";
+
+
+
+        //strFrac_float = convertFrac (strFracPart);
+        std::cout << "strFrac_float: " << strFrac_float << "\n";
+    }
+
+
+    return (strInt_int + strFrac_float);
+
+}
+
+
+   /* for (int i = endPos; i <= str.length(); i--) {
+        std::cout  << "i = " << i << std::endl;
+       }*/
 
 
 
 
 int main() {
 
-    std::string str = "";
+    std::string str_first = "";
+    std::string str_second = "";
 
-    std::cout << "Enter email\n=>";
-    std::getline(std::cin, str);
+    std::cout << "Enter first number\n=>";
+    std::getline(std::cin, str_first);
 
-    int correct = 0;
-    bool minus = (str.length() > 0 && str [0] == '-');
+    std::cout << "Enter second number\n=>";
+    std::getline(std::cin, str_second);
+
+    std::string strIntPart_first = get_intPart (str_first);
+    std::string strFracPart_first = get_fracPart(str_first);
+
+    std::string strIntPart_second = get_intPart (str_second);
+    std::string strFracPart_second = get_fracPart(str_second);
+
+    std::cout << "strIntPart_first: " << strIntPart_first << "\n";
+    std::cout << "strFracPart_first: " << strFracPart_first << "\n";
+
+    std::cout << "strIntPart_first: " << strIntPart_second << "\n";
+    std::cout << "strFracPart_first: " << strFracPart_second << "\n";
+
+    bool correctEnter = (checkSymbol (strIntPart_first) &&
+                         checkSymbol (strFracPart_first) &&
+                         checkSymbol(strIntPart_second) &&
+                         checkSymbol(strFracPart_second));
+
+    std::cout << "correctSymbol: " << correctEnter << "\n";
+
+    if (!correctEnter) std::cout << "Input incorrect!\n";
+    else {
+        int strIntToInt_first = convertInt (strIntPart_first);
+        float strFracToFloat_first = convertFrac(strFracPart_first);
+        int strIntToInt_second = convertInt (strIntPart_second);
+        float strFracToFloat_second = convertFrac(strFracPart_second);
+
+        std::cout << "strIntToInt_first: " << strIntToInt_first << "\n";
+        std::cout << "strFracToFloat_first: " << strFracToFloat_first << "\n";
+        std::cout << "strIntToInt_second: " << strIntToInt_second << "\n";
+        std::cout << "strFracToFloat_second: " << strFracToFloat_second << "\n";
+    }
+
+    // std::string strIntPart_first = "";
+   // std::string strFracPart_second = "";
+
+   // int str_int = 0;
+    //int strIntPart_int = 0;
+   // float strFracPart_float = 0;
+
+    //bool correct = (correctEnter(str_first) && correctEnter(str_second));
+
+   //    bool correct = (correctEnter(str_first) && (correctEnter(str_second)));
+
+/*
+
     int dot = find_dot(str);
-    //std::cout << "DOT - " << dot << "\n";
-
+    //std::cout << "DOT:  " << dot << "\n";
 
     if (dot == -1) {
-        correct = check(str, minus);
-        std::cout << "correct83 - " << correct << "\n";
+        cor = check(str, minus);
+        //std::cout << "correct68 - " << correct << "\n";
+        //std::cout << "str - " << str << "\n";
+
     }
     else {
-        std::string strFirst = get_firstPart (str, dot);
-        std::cout << "strFirst - " << strFirst << "\n";
-
-        std::string strSecond = get_secondPart (str, dot);
-        std::cout << "strSecond - " << strSecond << "\n";
-
-        correct = (check(strFirst, minus) && check(strFirst, false));
-        std::cout << "correct88 - " << correct << "\n";
-
-
+        strIntPart = get_intPart (str, dot);
+        std::cout << "strIntPart: " << strIntPart << "\n";
+        strFracPart = get_fracPart(str, dot);
+        std::cout << "strFracPart: " << strFracPart << "\n";
+        correct = (check(strIntPart, minus) && check(strFracPart, false));
+        //std::cout << "correct78 - " << correct << "\n";
     }
 
 
+
+
+
+
+
+
+
+
+    str_int = convertInt (str, minus);
+    std::cout << "str_int: " << str_int << "\n";
+
+
+   */
+
+//========================================
+
+/*
     if (!correct) {
         std::cout << "Input incorrect!\n";
     }
     else {
+
+
+
         std::cout << "GOOD!\n";
+
+        float str1;
+        float str2;
+
+        str1 = convert(str_first);
+        str2 = convert(str_second);
+
+        std::cout << "str1   " << str1 << "\n";
+        std::cout << "str2   " << str2 << "\n";
+
+
+        //strIntPart_int = convertInt (strIntPart, minus);
+        //std::cout << "strIntPart_int: " << strIntPart_int << "\n";
+        //strFracPart_float = convertFrac (strFracPart);
+        //std::cout << "strSecond_int: " << strFracPart_float << "\n";
+
+
+
+        bool more;
+        if (str1 > str2)
+        {
+            std::cout << "more\n";
+        }
+        else
+        std::cout << "less\n";
+
+
+          //  more = (strIntPart_int > strFracPart_int) ? true : false;
+        //else
+        //{
+
+        //}
+
+
+
+
+
+
     }
-
-
-
-
-
+*/
     return 0;
 }
 

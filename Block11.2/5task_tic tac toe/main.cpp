a3f9f369a7387a19bfab97d41883e3709eed16a5
Orphan72 <sdenko17@gmail.com>
2021-06-29 09:13:47 +0300

    210629_task5

--- Block11.2/5task_tic tac toe/main.cpp
+++ Block11.2/5task_tic tac toe/main.cpp
@@ -1,8 +1,74 @@
 #include <iostream>
+#include <utility>
+
+char get_elmnt (int x, int y, std::string str1, std::string str2, std::string str3)
+{
+    //check correct enter x and y
+    std::string line = "";
+    if (y == 1)
+        line = str1;
+    else if (y == 2)
+        line = str2;
+    else
+        line = str3;
+    return line [x-1];
+}
+
 
 int main() {
 
-   std::cout << "Start";
+   std::string str_first;
+   std::string str_secnd;
+   std::string str_third;
+
+   std::cout << "Enter the first line\n=>";
+   std::getline(std::cin, str_first);
+
+   std::cout << "Enter the second line\n=>";
+   std::getline(std::cin, str_secnd);
+
+   std::cout << "Enter the third line\n=>";
+   std::getline(std::cin, str_third);
+
+   //запустить в цикле get_elmnt чтобы вывести на экран
+  std::cout << "-------------\n";
+   for (int yy = 1; yy < 4; yy++)
+   {
+       for (int xx = 1; xx < 4; xx++)
+       {
+            std::cout << "| " << get_elmnt (xx, yy, str_first, str_secnd, str_third) << " ";
+       }
+
+       std::cout << "|\n";
+       std::cout << "-------------\n";
+   }
+
+    char element = get_elmnt(3,1,str_first, str_secnd, str_third);
+
+    std::cout << "element: " << element << std::endl;
+    int count = 0;
+    for (int i = 1; i < 4; i++) {
+
+        if ((get_elmnt(1, i, str_first, str_secnd, str_third) ==
+             get_elmnt(2, i, str_first, str_secnd, str_third)) &&
+            (get_elmnt(2, i, str_first, str_secnd, str_third) ==
+             get_elmnt(3, i, str_first, str_secnd, str_third)))
+        {
+            count++;
+            std::cout << get_elmnt(1, i, str_first, str_secnd, str_third);
+            std::cout << "   WIIIIN!!!\n";
+            std::cout << count;
+        }
+
+    }
+
+
+
+
+
+
+
+
     return 0;
 }
 

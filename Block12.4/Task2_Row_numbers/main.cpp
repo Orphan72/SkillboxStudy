c544ca1dbcaa474932c113fbaae309ce289e8952
Orphan72 <sdenko17@gmail.com>
2021-07-18 18:35:55 +0300

    210718_Task2

--- Block12.4/Task2_Row_numbers/main.cpp
+++ Block12.4/Task2_Row_numbers/main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    int numbers [] = {114, 111, 106, 107, 108, 105, 115, 108, 110, 109, 112, 113, 116, 117, 118};
+    int newNumbers [15];
+    int min = numbers [0];
+    for (int i = 0; i < 15; i++)
+    {
+        if (numbers [i] < min)
+            min = numbers [i];
+    }
+
+    std::cout << "min: " << min << std::endl;
+    for (int i = 0; i < 15; i++)
+    {
+        newNumbers [i] = min;
+        min++;
+    }
+
+    for (int i = 0; i < 15; i++)
+    {
+        std::cout << i << ": " << newNumbers [i] << std::endl;
+    }
+
+
     return 0;
 }

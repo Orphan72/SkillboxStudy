e855e7801bbcbaff78ff1f4afe5c8b86019b0dbf
Orphan72 <sdenko17@gmail.com>
2021-07-21 07:49:18 +0300

    finishTask2

--- Block12.4/Task2_Row_numbers/main.cpp
+++ Block12.4/Task2_Row_numbers/main.cpp
@@ -1,28 +1,40 @@
 #include <iostream>
 
+const int N = 15;
+int getMin (int num [N]) {
+    int min = num [0];
+    for (int i = 0; i < N; i++)
+    {
+        if (num [i] < min)
+            min = num [i];
+    }
+   return min;
+ }
+
+int getDouble (int min, int num [N]) {
+     for (int i = 0; i < N; i++) {
+        for (int j = 0; j < 7; j++) {
+            if (num [j] == min) {
+                num[j] = 0;
+                break;
+             }
+         }
+         min++;
+     }
+     for (int i = 0; i < N; i++) {
+         if (num [i] != 0)
+             return num [i];
+     }
+    return 0;
+  }
+
 int main() {
 
-    int numbers [] = {114, 111, 106, 107, 108, 105, 115, 108, 110, 109, 112, 113, 116, 117, 118};
-    int newNumbers [15];
-    int min = numbers [0];
-    for (int i = 0; i < 15; i++)
-    {
-        if (numbers [i] < min)
-            min = numbers [i];
-    }
-
-    std::cout << "min: " << min << std::endl;
-    for (int i = 0; i < 15; i++)
-    {
-        newNumbers [i] = min;
-        min++;
-    }
-
-    for (int i = 0; i < 15; i++)
-    {
-        std::cout << i << ": " << newNumbers [i] << std::endl;
-    }
-
-
+    int numbers[15] = {114,111, 106, 107, 108, 105, 115,  108, 110, 109, 112, 113, 116, 117, 118};
+    int minimum = getMin(numbers);
+    int doubled = 0;
+    std::cout << "min: " << minimum << std::endl;
+    doubled = getDouble(minimum, numbers);
+    std::cout << "doubled: " << doubled << std::endl;
     return 0;
 }

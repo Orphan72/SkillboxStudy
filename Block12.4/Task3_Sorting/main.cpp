5af9b0f33af23ca30187be708e64c7cc25c9f131
Orphan72 <sdenko17@gmail.com>
2021-07-22 21:32:20 +0300

    210722Task3

--- Block12.4/Task3_Sorting/main.cpp
+++ Block12.4/Task3_Sorting/main.cpp
@@ -1,8 +1,209 @@
 #include <iostream>
 
+int const N = 15;
+int getMin (float num [N]) {
+    int min = num [0];
+    for (int i = 0; i < N; i++)
+    {
+        if (num [i] < min)
+            min = num [i];
+    }
+    return min;
+}
+
+
+
 int main() {
 
-    float number [15] =  {1.2, 2.3, 1.11, 3.4, 5.5, 5.4, 5.3, 5.1, 1.5, 1.25, 5.41, 5.31, 5.11, 1.51, 1.251};
+    int number [15] =  {5, 7, 10, 3, 1, 4, 12, 8, 15, 14, 2, 13, 6, 11, 9};
+    //.2, 2.3, 1.11, 3.4, 5.5, 5.4, 5.3, 5.1, 1.5, 1.25, 5.41, 5.31, 5.11, 1.51, 1.251};
+
+
+    int min = 0;
+    int pos = 0;
+
+    for (int i = 0; i < 15; i++)
+    {
+        std::cout << number [i] << ", ";
+    }
+    std::cout << std::endl;
+
+
+   // for (int j = 0; j < 15; j++) {
+        min = number[0];
+        for (int i = 0; i < N; i++) {
+            if (number[i] < min) {
+                min = number[i];
+                pos = i;
+            }
+
+        }
+        std::cout << "min   " << min << std::endl;
+        std::cout << "pos   " << pos << std::endl;
+        std::cout << "number [pos] " << number [pos] << std::endl;
+        number[pos] = number[0];
+        number[0] = min;
+
+
+
+
+        for (int i = 0; i < 15; i++)
+        {
+            std::cout << number [i] << ", ";
+        }
+        std::cout << std::endl;
+
+
+
+
+    min = number[1];
+    for (int i = 1; i < N; i++) {
+        if (number[i] < min) {
+            min = number[i];
+            pos = i;
+        }
+
+    }
+    std::cout << "min   " << min << std::endl;
+    std::cout << "pos   " << pos << std::endl;
+    std::cout << "number [pos] " << number [pos] << std::endl;
+    number[pos] = number[1];
+    number[1] = min;
+
+
+    for (int i = 0; i < 15; i++)
+    {
+        std::cout << number [i] << ", ";
+    }
+    std::cout << std::endl;
+
+
+    min = number[2];
+    for (int i = 2; i < N; i++) {
+        if (number[i] < min) {
+            min = number[i];
+            pos = i;
+        }
+
+    }
+    std::cout << "min   " << min << std::endl;
+    std::cout << "pos   " << pos << std::endl;
+    std::cout << "number [pos] " << number [pos] << std::endl;
+
+    number[pos] = number[2];
+    number[2] = min;
+
+
+    for (int i = 0; i < 15; i++)
+    {
+        std::cout << number [i] << ", ";
+    }
+    std::cout << std::endl;
+
+
+
+
+    min = number[3];
+    for (int i = 3; i < N; i++) {
+        if (number[i] < min) {
+            min = number[i];
+            pos = i;
+        }
+
+    }
+    std::cout << "min   " << min << std::endl;
+    std::cout << "pos   " << pos << std::endl;
+    std::cout << "number [pos] " << number [pos] << std::endl;
+
+    number[pos] = number[3];
+    number[3] = min;
+
+
+    for (int i = 0; i < 15; i++)
+    {
+        std::cout << number [i] << ", ";
+    }
+    std::cout << std::endl;
+
+
+
+
+    min = number[4];
+    for (int i = 4; i < 15; i++) {
+        if (number[i] <= min) {
+            min = number[i];
+            pos = i;
+        }
+
+    }
+
+    std::cout << "min   " << min << std::endl;
+    std::cout << "pos   " << pos << std::endl;
+    std::cout << "number [pos] " << number [pos] << std::endl;
+    number[pos] = number[4];
+    number[4] = min;
+
+
+    for (int i = 0; i < 15; i++)
+    {
+        std::cout << number [i] << ", ";
+    }
+    std::cout << std::endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //}
+
+
+
+/*
+    min = number[1];
+    for (int i = 1; i < N; i++) {
+        if (number[i] < min) {
+            min = number[i];
+            pos = i;
+        }
+
+    }
+    number[pos] = number[1];
+    number[1] = min;
+
+*/
+
+    //std::cout << "min " << min;
+    //std::cout << "number " << number [j] << std::endl;
+
+
+
+
+
+
+
+
+
+
+
+
+    /*
+
+
+
     for (int j = 0; j < 15; j++)
     {
         for (int i = 0; i < 14; i++) {
@@ -25,6 +226,10 @@ int main() {
 
     }
 
+
+
+
+*/
 
 
 

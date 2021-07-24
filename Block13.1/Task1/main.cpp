9b889e0ff2a737a293975b270b0fb997d9982f3c
Orphan72 <sdenko17@gmail.com>
2021-07-24 22:58:24 +0300

    210724Task1

--- Block13.1/Task1/main.cpp
+++ Block13.1/Task1/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <vector>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int N = 0;
+    int sum = 0;
+    std::cout << "Enter amount of numbers\n=>";
+    std::cin >> N;
+    std::vector <int> vec (N);
+    std::cout << "Enter numbers\n";
+    for (int i = 0; i < N; i++){
+        std::cout << "=>";
+        std::cin >> vec [i];
+        sum += vec [i];
+    }
+    float average = sum/N;
+    std::cout << "Average is " << average << std::endl;
+
+ return 0;
 }
+
+
+/*
+Пользователь вводит с клавиатуры число n, а затем -- n целых чисел.
+Необходимо вычислить их среднее арифметическое и вывести на экран.
+ */

539dfbd8e4d5c196cff6ab65be2dab017e581e52
Orphan72 <sdenko17@gmail.com>
2021-07-27 07:44:14 +0300

    renameDirectory

---
+++ Block13.1/Task1_average/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    int N = 0;
+    int sum = 0;
+    std::cout << "Enter amount of numbers\n=>";
+    std::cin >> N;
+    std::vector <int> vec (N);
+    std::cout << "Enter numbers\n";
+    for (int i = 0; i < N; i++){
+        std::cout << "=>";
+        std::cin >> vec [i];
+        sum += vec [i];
+    }
+    float average = sum/N;
+    std::cout << "Average is " << average << std::endl;
+
+ return 0;
+}
+

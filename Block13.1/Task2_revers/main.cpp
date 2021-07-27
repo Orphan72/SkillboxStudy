87c0c662be20a20d62cf13e8995bcbeab434f9e5
Orphan72 <sdenko17@gmail.com>
2021-07-27 07:57:53 +0300

    finish Task2

--- Block13.1/Task2_revers/main.cpp
+++ Block13.1/Task2_revers/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <vector>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    int n = 0;
+    std::cout << "Enter amount of numbers\n=>";
+    std::cin >> n;
+    std::vector <float> vec (n);
+    for (int i = 0; i < n; i++){
+        std::cout << "=>";
+        std::cin >> vec [i];
+    }
+
+    for (int i = n - 1; i >= 0; i--){
+        std::cout << vec [i] << ", ";
+        }
+    std::cout << std::endl;
     return 0;
 }

36f37fc4b4f210e3ac5c4889a75a6ece5952fab2
Orphan72 <sdenko17@gmail.com>
2021-07-30 21:40:53 +0300

    B13.3.1Robet sell continue

--- Block13.3/Task1_sell robots/main.cpp
+++ Block13.3/Task1_sell robots/main.cpp
@@ -3,6 +3,7 @@
 
 int main() {
     int num = 0;
+    int index = 0;
     std::cout << "Enter count of robots\n=>";
     std::cin >> num;
     std::vector <int> vec (num);
@@ -17,6 +18,23 @@ int main() {
     for (int i = 0; i < vec.size(); i++){
         std::cout << vec [i] << " ";
     }
+
+    std::cout << "Enter index:\n";
+    std::cin >> index;
+    //index++;
+
+    vec.resize(vec.size()-1);
+
+    for (int i = index - 1 ; i < vec.size(); i++){
+        vec [i] = vec [i+1];
+    }
+
+
+    for (int i = 0; i < vec.size(); i++){
+        std::cout << vec [i] << " ";
+    }
+
+
 
 
     return 0;

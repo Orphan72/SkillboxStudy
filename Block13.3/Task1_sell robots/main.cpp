40655bfecf7746ce76ac91d01313cf31a0f8e15b
Orphan72 <sdenko17@gmail.com>
2021-07-31 22:04:41 +0300

    B13.3.1Finish \Task1SellsRobots

--- Block13.3/Task1_sell robots/main.cpp
+++ Block13.3/Task1_sell robots/main.cpp
@@ -14,28 +14,32 @@ int main() {
     }
 
     std::cout << "There are robots at the showcase:\n";
-
     for (int i = 0; i < vec.size(); i++){
         std::cout << vec [i] << " ";
     }
 
-    std::cout << "Enter index:\n";
-    std::cin >> index;
-    //index++;
-
-    vec.resize(vec.size()-1);
-
-    for (int i = index - 1 ; i < vec.size(); i++){
-        vec [i] = vec [i+1];
+    while (vec.size() != 0) {
+        std::cout << "Enter index:\n";
+        bool correncIndex = false;
+        while (!correncIndex) {
+            std::cin >> index;
+            correncIndex = (index > 0 && index <= vec.size());
+            if (!correncIndex) {
+                std::cout << "index incorrect! Try again\n";
+            }
+        }
+            vec.resize(vec.size() - 1);
+            for (int i = index - 1; i < vec.size(); i++) {
+                vec[i] = vec[i + 1];
+            }
+            for (int i = 0; i < vec.size(); i++) {
+                std::cout << vec[i] << " ";
+            }
     }
 
-
-    for (int i = 0; i < vec.size(); i++){
-        std::cout << vec [i] << " ";
+    if (vec.size() == 0){
+        std::cout << "All robots were sold";
     }
-
-
-
 
     return 0;
 }

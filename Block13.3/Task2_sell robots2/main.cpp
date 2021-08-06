fff6d98fc87bdd13fc0aaba49f69066082920eee
Orphan72 <sdenko17@gmail.com>
2021-08-06 07:36:45 +0300

    fixTask13.3.2 Sell robots2

--- Block13.3/Task2_sell robots2/main.cpp
+++ Block13.3/Task2_sell robots2/main.cpp
@@ -58,11 +58,11 @@ int main() {
                     std::cout << "index incorrect! Try again\n";
                 }
             }
-            vec.resize(vec.size() - 1);
-            for (int i = index - 1; i < vec.size(); i++) {
+            for (int i = (index - 1); i < (vec.size() - 1); i++) {
                 vec[i] = vec[i + 1];
             }
 
+			vec.resize(vec.size() - 1);
             for (int i = 0; i < vec.size(); i++) {
                std::cout << vec[i] << " ";
             }

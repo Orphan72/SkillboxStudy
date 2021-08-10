c272159cd90c8fb313a07108d5ee3f3d4f1a4930
Orphan72 <sdenko17@gmail.com>
2021-08-10 08:41:16 +0300

    Finish task 1 HW13

--- Block13.5/HW1_numbers/main.cpp
+++ Block13.5/HW1_numbers/main.cpp
@@ -3,6 +3,7 @@
 
 int main() {
     int count = 0;
+    int num = 0;
     std::cout << "Enter count of elements\n=>";
     std::cin >> count;
     std::vector<int> vec (count);
@@ -12,38 +13,31 @@ int main() {
         std::cin >> vec [i];
     }
 
+    std::cout << "Initial vector:\n";
     for (int i = 0; i < vec.size(); i++){
         std::cout << vec [i] << " ";
     }
-
-    int num = 0;
-    int index = 0;
     std::cout << "Enter number\n=>";
     std::cin >> num;
 
-    for (int i = 0; i < vec.size(); i++) {
-        if (vec.size() == 1 && vec[0] == num) {
-            vec.clear();
-        }
-        else {
-            while (vec[i] == num) {
-                index = i;
-                for (int j = index; j < vec.size() - 1; j++) {
-                    vec[j] = vec[j + 1];
-                }
-                vec.pop_back();
+    for (int i = vec.size() - 1 ; i >= 0; i--) {
+        if (vec [i] == num){
+            for (int j = i; j < vec.size(); j++) {
+                vec [j] = vec [j + 1];
             }
+            vec.pop_back();
         }
     }
 
-    if (vec.empty()){
+    std::cout << "Final vector:\n";
+    if (vec.empty()) {
         std::cout << "Vector is empty\n";
     }
     else {
-        for (int i = 0; i < vec.size(); i++) {
-            std::cout << vec[i] << " ";
+        for (int i = 0; i < vec.size(); i++){
+            std::cout << vec [i] << " ";
         }
-    }
+     }
 
     return 0;
 }

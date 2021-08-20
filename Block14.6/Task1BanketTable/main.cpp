38664636ee3d8f751423eb4c74da23350c7680b3
Orphan72 <sdenko17@gmail.com>
2021-08-20 15:47:08 +0300

    Task1 initVectors

--- Block14.6/Task1BanketTable/main.cpp
+++ Block14.6/Task1BanketTable/main.cpp
@@ -1,41 +1,91 @@
 #include <iostream>
 #include <vector>
 
+
+void display (std::vector<std::vector<std::string>> vec)
+{
+    for (int i = 0; i < vec.size(); i++) {
+        for (int j = 0; j < vec[i].size(); j++) {
+            std::cout << vec [i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+void displayChair (std::vector<std::vector<bool>> vec)
+{
+    for (int i = 0; i < vec.size(); i++) {
+        for (int j = 0; j < vec[i].size(); j++) {
+            std::cout << vec [i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+void displayVipChair (std::vector<bool> vec)
+{
+    for (int i = 0; i < vec.size(); i++) {
+        std::cout << vec [i] << " ";
+    }
+        std::cout << std::endl;
+}
+
+
+
+
 int main() {
 
-std::vector <std::vector<std::string>> cultery {{"spoon", "fork", "knife"},
-                                                {"spoon", "fork", "knife"},
-                                                {"spoon", "fork", "knife"},
-                                                {"spoon", "fork", "knife"},
-                                                {"spoon", "fork", "knife"},
-                                                {"spoon", "fork", "knife"},
-                                                {"spoon", "fork", "knife"},
-                                                {"spoon", "fork", "knife"},
-                                                {"spoon", "fork", "knife"},
-                                                {"spoon", "fork", "knife"},
-                                                {"spoon", "fork", "knife", "desspoon"},
-                                                {"spoon", "fork", "knife", "desspoon"}};
+    const int ROW      = 2;
+    const int ECONOM   = 5;
+    const int VIP      = 2;
+    const int ECCULT   = 3;
+    const int VIPCULT  = 4;
+    const int ECPLATE  = 2;
+    const int VIPPLATE = 3;
 
-std::vector <std::vector<std::string>> tableware {{"fDish", "secDish"},
-                                                  {"fDish", "secDish"},
-                                                  {"fDish", "secDish"},
-                                                  {"fDish", "secDish"},
-                                                  {"fDish", "secDish"},
-                                                  {"fDish", "secDish"},
-                                                  {"fDish", "secDish"},
-                                                  {"fDish", "secDish"},
-                                                  {"fDish", "secDish"},
-                                                  {"fDish", "secDish"},
-                                                  {"fDish", "secDish", "desDish"},
-                                                  {"fDish", "secDish", "desDish"}};
+    std::vector <std::vector<std::string>> econCultery (ECONOM*ROW, std::vector<std::string> (ECCULT));
+    std::vector <std::vector<std::string>> econPlate (ECONOM*ROW, std::vector<std::string> (ECPLATE));
+    std::vector <std::vector<std::string>> vipCultery (VIP, std::vector<std::string> (VIPCULT));
+    std::vector <std::vector<std::string>> vipPlate (VIP, std::vector<std::string> (VIPPLATE));
+
+    std::vector <std::vector<bool>> econChair (ROW, std::vector<bool> (ECONOM));
+    std::vector <bool> vipChair (VIP);
+
+    for (int i = 0; i < econCultery.size(); i++) {
+        econCultery [i] = {"spoon", "fork", "knife"};
+    }
+
+    for (int i = 0; i < econPlate.size(); i++) {
+        econPlate [i] = {"plateOne", "plateSec"};
+    }
+
+    for (int i = 0; i < vipCultery.size(); i++) {
+        vipCultery [i] = {"spoon", "fork", "knife", "desSpoon"};
+    }
+
+    for (int i = 0; i < vipPlate.size(); i++) {
+        vipPlate [i] = {"firstPlate", "sesPlate", "desPlate"};
+    }
+
+    for (int i = 0; i < ROW; i++) {
+        for (int j = 0; j < ECONOM; j++) {
+                econChair [i][j] = true;
+        }
+    }
+
+    for (int i = 0; i < VIP; i++) {
+        vipChair [i] = true;
+    }
+
+   display (econCultery);
+   display (econPlate);
+   display (vipCultery);
+   display (vipPlate);
+   displayChair (econChair);
+   displayVipChair (vipChair);
 
 
-std::vector <std::vector<bool>> chair {{true, true, true, true, true, true, true, true, true, true},
-                                       {true, true, true, true, true, true, true, true, true, true},
-                                       {true, true}};
-
-
-/*
+    /*
 
     По поводу успешного прохождения двумерных массивов собирается банкет, организуется стол на 12 персон, 2 из них - это места VIP,
     они расположены рядом друг к другу во главе стола. Стол протяжённый в длину, места расположены в два ряда.

29de662b840f031999d9ad7e78065dfeb716bfd2
Orphan72 <sdenko17@gmail.com>
2021-08-25 16:41:12 +0300

    B14T2 continue

--- Block14.6/Task2TicTacToe/main.cpp
+++ Block14.6/Task2TicTacToe/main.cpp
@@ -10,9 +10,48 @@ void displayField (char vec [SIZE][SIZE]){
     }
 }
 
+
+char checkWin (char vec [SIZE][SIZE])
+{
+    char winner = 'N';
+    for (int i = 0; i < SIZE; i++)
+    {
+        if (vec [i][0] == vec [i][1] && vec [i][1] == vec [i][2] && vec [i][0] != '5')
+        {
+            winner = vec [i][0];
+            return winner;
+        }
+        else if  (vec [0][i] == vec [1][i] && vec [1][i] == vec [2][i] && vec [0][i] != '5')
+        {
+            winner = vec [i][0];
+            return winner;
+        }
+    }
+    return winner;
+}
+
+
+bool searchGap (char vec [SIZE][SIZE])
+{
+    int countGap = 0;
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; i++)
+        {
+            if (vec [i][j] == ' ')
+                countGap++;
+        }
+    }
+
+    if (countGap == 0)
+        return false;
+}
+
 int main() {
 
    char field [SIZE][SIZE];
+   int x = 0;
+   int y = 0;
+
 
    for (int i = 0; i < SIZE; i++) {
        for (int j = 0; j < SIZE; j++) {
@@ -20,6 +59,53 @@ int main() {
        }
    }
    displayField (field);
+   char win = ' ';
+
+   //todo
+   bool gap = true;
+
+
+   //todo - while, use search gap
+   for (int i = 0; i < 4; i++) {
+
+       gap = searchGap (field);
+       std::cout << "Player X, your turn. Input coordinates\n=>";
+       std::cin >> x;
+       std::cout << "=>";
+       std::cin >> y;
+
+       field[y - 1][x - 1] = 'X';
+
+       displayField (field);
+
+       win = checkWin(field);
+       if (win) {
+           std::cout << "Stop!\n";
+           break;
+       }
+
+
+       std::cout << "Player O, your turn. Input coordinates\n=>";
+       std::cin >> x;
+       std::cout << "=>";
+       std::cin >> y;
+
+       field[y - 1][x - 1] = 'O';
+       displayField (field);
+
+       win = checkWin(field);
+       if (win) {
+           std::cout << "Stop!\n";
+           break;
+       }
+   }
+
+   std::cout << "Winner is " << win << std::endl;
+
+
+
+
+
    return 0;
 }
 

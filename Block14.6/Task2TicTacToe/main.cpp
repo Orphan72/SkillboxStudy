d683ec0e9f9aa7e2e5c8a2670b20ba6192ed0099
Orphan72 <sdenko17@gmail.com>
2021-08-27 13:13:07 +0300

    B14T2 finish

--- Block14.6/Task2TicTacToe/main.cpp
+++ Block14.6/Task2TicTacToe/main.cpp
@@ -1,111 +1,111 @@
 #include <iostream>
 const int SIZE = 3;
 
-void displayField (char vec [SIZE][SIZE]){
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
+void displayField (char vec [SIZE][SIZE])
+{
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 0; j < SIZE; j++)
+        {
             std::cout << vec [i][j] << " | ";
         }
         std::cout << " \n-----------\n";
     }
 }
 
-
 char checkWin (char vec [SIZE][SIZE])
 {
     char winner = 'N';
     for (int i = 0; i < SIZE; i++)
     {
-        if (vec [i][0] == vec [i][1] && vec [i][1] == vec [i][2] && vec [i][0] != '5')
+        int k = 0;
+        int n = 0;
+        if (vec[i][k] == vec[i][++k] && vec[i][k] == vec[i][++k] && vec[i][k] != ' ')
         {
-            winner = vec [i][0];
+            winner = vec[i][k];
             return winner;
         }
-        else if  (vec [0][i] == vec [1][i] && vec [1][i] == vec [2][i] && vec [0][i] != '5')
+
+        else if  (vec [n][i] == vec [++n][i] && vec [n][i] == vec [++n][i] && vec [n][i] != ' ')
         {
-            winner = vec [i][0];
+            winner = vec [n][i];
             return winner;
         }
     }
     return winner;
 }
 
-
-bool searchGap (char vec [SIZE][SIZE])
+int main()
 {
-    int countGap = 0;
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; i++)
-        {
-            if (vec [i][j] == ' ')
-                countGap++;
-        }
-    }
-
-    if (countGap == 0)
-        return false;
-}
-
-int main() {
-
    char field [SIZE][SIZE];
-   int x = 0;
-   int y = 0;
+   int row = 0;
+   int col = 0;
+   int turnCounter = 0;
+   char win = 'N';
 
-
-   for (int i = 0; i < SIZE; i++) {
-       for (int j = 0; j < SIZE; j++) {
-            field [i][j] = '5';
+   for (int i = 0; i < SIZE; i++)
+   {
+       for (int j = 0; j < SIZE; j++)
+       {
+            field [i][j] = ' ';
        }
    }
    displayField (field);
-   char win = ' ';
 
-   //todo
-   bool gap = true;
-
-
-   //todo - while, use search gap
-   for (int i = 0; i < 4; i++) {
-
-       gap = searchGap (field);
-       std::cout << "Player X, your turn. Input coordinates\n=>";
-       std::cin >> x;
-       std::cout << "=>";
-       std::cin >> y;
-
-       field[y - 1][x - 1] = 'X';
-
-       displayField (field);
-
-       win = checkWin(field);
-       if (win) {
-           std::cout << "Stop!\n";
-           break;
+   while (true)
+   {
+       bool correct = false;
+       while (!correct)
+       {
+           std::cout << "Player X, your turn. Input row\n=>";
+           std::cin >> row;
+           std::cout << "Input column\n=>";
+           std::cin >> col;
+           correct = (row > 0 && row <= SIZE) && (col > 0 && col <= SIZE);
+           if (!correct)
+           {
+               std::cout << "incorrect input. Please, try again\n";
+           }
+           else if (field[row - 1][col - 1] != ' ')
+           {
+               std::cout << "This cell is busy. Please, try again\n";
+               correct = false;
+           }
        }
 
-
-       std::cout << "Player O, your turn. Input coordinates\n=>";
-       std::cin >> x;
-       std::cout << "=>";
-       std::cin >> y;
-
-       field[y - 1][x - 1] = 'O';
+       field[row - 1][col - 1] = 'X';
        displayField (field);
-
        win = checkWin(field);
-       if (win) {
-           std::cout << "Stop!\n";
-           break;
+       turnCounter++;
+       if (win == 'X' || turnCounter == SIZE*SIZE) break;
+       else {
+           correct = false;
+           while (!correct) {
+               std::cout << "Player O, your turn. Input row\n=>";
+               std::cin >> row;
+               std::cout << "Input column\n=>";
+               std::cin >> col;
+               correct = (row > 0 && row <= SIZE) && (col > 0 && col <= SIZE);
+               if (!correct) {
+                   std::cout << "incorrect input. Please, try again\n";
+               } else if (field[row - 1][col - 1] != ' ') {
+                   std::cout << "This cell is busy. Please, try again\n";
+                   correct = false;
+               }
+           }
+           field[row - 1][col - 1] = 'O';
+           displayField(field);
+           win = checkWin(field);
+           turnCounter++;
+           if (win == 'O')
+               break;
        }
    }
 
-   std::cout << "Winner is " << win << std::endl;
-
-
-
-
-
+   if (win == 'N')
+       std::cout << "Nobody won\n ";
+   else
+       std::cout << "Player " << win << " won!\n";
    return 0;
 }
 

3836ff366d82acb9bd80596d70a7ab22c5e88b91
Orphan72 <sdenko17@gmail.com>
2021-08-31 12:56:05 +0300

    B14T3 snake fix

--- Block14.6/Task3Snake/main.cpp
+++ Block14.6/Task3Snake/main.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 
 const int SIZE = 5;
-
 void display (int mas [SIZE][SIZE])
 {
-    for (int i = 0; i < SIZE; i++)
+    for (int k = 0; k < SIZE; k++)
     {
-        for (int j = 0; j < SIZE; j++)
+        for (int n = 0; n < SIZE; n++)
         {
-            std::cout << mas [i][j] << " ";
+            std::cout << mas[k][n] << " ";
         }
         std::cout << std::endl;
     }
@@ -16,13 +15,14 @@ void display (int mas [SIZE][SIZE])
 
 int main()
 {
+
     int snake [SIZE][SIZE];
     int count = 0;
 
     int i = -1;
     int j = -1;
-    for (int n = 0; n <= SIZE/2 ; n++)
-    {
+
+
         i++;
         j++;
         for (; j < SIZE; j++)
@@ -36,7 +36,30 @@ int main()
         {
             snake[i][j] = count++;
         }
-    }
+
+        i++;
+        j++;
+        for (; j < SIZE; j++)
+        {
+            snake[i][j] = count++;
+        }
+
+        i++;
+        j--;
+        for (; j >= 0; j--)
+        {
+            snake[i][j] = count++;
+        }
+
+        i++;
+        j++;
+        for (; j < SIZE; j++)
+        {
+            snake[i][j] = count++;
+        }
+
+
+    display (snake);
 
     return 0;
 }

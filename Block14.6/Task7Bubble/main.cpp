88686d0a09b13904554168bf13f8c6ea2901ad2d
Orphan72 <sdenko17@gmail.com>
2021-09-05 14:01:58 +0300

    B14T7 bubble finish

--- Block14.6/Task7Bubble/main.cpp
+++ Block14.6/Task7Bubble/main.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <valarray>
 
-const int SIZE = 5;
+const int SIZE = 12;
 
 void display (std::vector <std::vector <bool>> vec)
 {
@@ -27,9 +27,14 @@ std::vector<std::vector <bool>> pop (std::vector<std::vector <bool>> vec, int st
     {
         for (int j = (stX - 1); j < finX; j++)
         {
-            vec [i][j] = false;
+            if (vec[i][j])
+            {
+                vec [i][j] = false;
+                std::cout << "Pop! ";
+            }
         }
     }
+    std::cout << std::endl;
     return vec;
 }
 
@@ -60,7 +65,7 @@ int main() {
     {
         for (int j = 0; j < SIZE; j++)
         {
-            bubble [i][j] = true;
+            bubble[i][j] = true;
         }
     }
 
@@ -71,12 +76,12 @@ int main() {
         bool correct = false;
         while (!correct)
         {
-            std::cout << "Please input start coordinates from 1 to 12\n=>";
+            std::cout << "Please input start coordinates from 1 to " << SIZE << std::endl;
             std::cin >> rowStart >> colStart;
-            correct = checkEnter (rowStart, colStart);
+            correct = checkEnter(rowStart, colStart);
             if (!correct)
             {
-                std::cout << "Values must be from 1 to 12. Please, try again\n";
+                std::cout << "Values must be from 1 to to " << SIZE <<  " Please, try again\n";
             }
         }
 
@@ -84,12 +89,12 @@ int main() {
 
         while (!correct)
         {
-            std::cout << "Please input finish coordinates from 1 to 12\n=>";
+            std::cout << "Please input finish coordinates from 1 to " << SIZE << std::endl;
             std::cin >> rowFin >> colFin;
             correct = checkEnter (rowFin, colFin);
             if (!correct)
             {
-                std::cout << "Values must be from 1 to 12. Please, try again\n";
+                std::cout << "Values must be from 1 to to " << SIZE <<  " Please, try again\n";
             }
         }
 
@@ -97,30 +102,12 @@ int main() {
 
         display(bubble);
 
-        //std::cout << "Enter intct\n";
         intact = checkIntact (bubble);
-        std::cout << "intct  " << intact;
+        if (!intact)
+        {
+            std::cout << "All bubbles are burst\n";
+        }
     }
 
     return 0;
 }
-
-/*
-Задача 7. Пупырка
-
-После посылки из Китая осталась спец. плёнка для бережной перевозки груза — пупырка.
-Всего в пупырке 12 на 12 шариков-пузырьков. Состояние любого пузырька — это всегда либо он целый, либо нет, т.е. true или false (тип массива bool).
-Для начала, требуется реализовать отдельную функцию инициализации пупырки начальным состоянием — полностью целая пупырка, т.е. все элементы true.
-Но это только самое начало.
-
-Программа заключается в последовательном лопанье целых регионов пузырьков.
-Перед каждым таким лопаньем надо показывать пользователю полностью всю плёнку: o — это целый пузырёк, x — лопнутый.
-Это тоже надо выделить в отдельную функцию. Итак, как выглядит само лопанье.
-Пользователь вводит две координаты: координаты начала региона и конца прямоугольного региона.
-Сама процедура лопанья пузырей должна быть реализована с помощью отдельной функции, все аргументы должны проверяться на их валидность,
-что они в в рамках диапазона возможных значений — иначе должна быть выведена ошибка.
-После лопанья каждого пузыря, который не был ещё лопнут до того, в консоль должно выводиться сообщение “Pop!”.
-
-Лопанье должно продолжаться до последнего пузырька. Как только вся пупырка потрачена, программа заканчивает выполнение.
-В принципе, вы можете подсчитать окончание в самой функции по отображению пузырьков плёнки, т.к. она выполняется после каждого лопанья.
- */

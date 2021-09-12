1f42a4860e938fb14f2f176501251a494f536faf
Orphan72 <sdenko17@gmail.com>
2021-09-12 16:05:41 +0300

    Signed-off-by: Orphan72 <sdenko17@gmail.com>

--- Block14.6/Task9Sea battle/main.cpp
+++ Block14.6/Task9Sea battle/main.cpp
@@ -18,53 +18,74 @@ void display (char array [SIZE][SIZE])
     std::cout << std::endl;
 }
 
-
-void setupShip (char array [SIZE][SIZE], int dsk)
+void setupShip (char array [SIZE][SIZE], int dsk, int cntShp)
 {
-    for (int count = 0; count < dsk; count++)
-    {
-        std::cout << "Enter coordinates " << count + 1 << " ship =>";
-        int horisontal, vertical;
-        char position = ' ';
-        std::cin >> horisontal >> vertical;
-        int i = horisontal + DELTA;
-        int j = vertical + DELTA;
-        bool correctPos = false;
-        while (!correctPos)
+   for (int n = 0; n < cntShp; n++)
+   {
+       char position = ' ';
+       if (dsk != 1)
        {
-            std::cout << "Enter ship's position (V or H)\n=>";
-            std::cin >> position;
-            correctPos = (position == 'V' || position == 'H');
-            if (position == 'V')
-            {
-                std::cout << "vertical\n";
-                for (; i < (horisontal + DELTA + dsk); i++)
-                {
-                    array[i][j] = 'O';
-                }
-            }
-            else if (position == 'H')
-            {
-                std::cout << "horisontal\n";
-                for (; j < (vertical + DELTA + dsk); j++)
-                {
-                    array[i][j] = 'O';
-                }
-            }
-            else
-               std::cout << "Position incorrect, please try again\n";
+           bool correctPos = false;
+           while (!correctPos)
+           {
+               std::cout << "Enter ship's position (V or H)\n=>";
+               std::cin >> position;
+               correctPos = (position == 'V' || position == 'H');
+               if (!correctPos)
+                   std::cout << "Position incorrect, please try again\n";
+           }
+       }
+       else
+       {
+           position = 'V';
        }
 
-       display (array);
-    }
+
+       bool correctCoordinates = false;
+
+       while (!correctCoordinates)
+       {
+           std::cout << "Enter coordinates ship =>";
+           int horisontal, vertical;
+           std::cin >> horisontal >> vertical;
+           correctCoordinates = (horisontal >= 0 && vertical >= 0 &&
+                                 horisontal <= 9 && vertical <= 9);
+           if (!correctCoordinates)
+           {
+               
+           }
+       }
+
+
+
+
+
+       int i = horisontal + DELTA;
+       int j = vertical + DELTA;
+
+       if (position == 'V')
+       {
+           for (; i < (horisontal + DELTA + dsk); i++)
+           {
+               array[i][j] = 'O';
+           }
+       }
+       else if (position == 'H')
+       {
+           for (; j < (vertical + DELTA + dsk); j++) {
+               array[i][j] = 'O';
+           }
+       }
+
+       display(array);
+   }
 }
-
-
 
 int main()
 {
     const int DIFF = 46;
-    //int desk = 3;
+    const int MAXDESK = 4;
+
     char field1 [SIZE][SIZE];
     char field2 [SIZE][SIZE];
 
@@ -84,38 +105,25 @@ int main()
 
     for (int i = DELTA; i < SIZE; i++)
     {
-        field1 [i][0] = i + DIFF;
-        field2 [i][0] = i + DIFF;
+        field1 [i][0] = char (i + DIFF);
+        field2 [i][0] = char (i + DIFF);
 
         for (int j = DELTA; j < SIZE; j++)
         {
-            field1 [0][j] = j + DIFF;
-            field2 [0][j] = j + DIFF;
-            field1 [i][j] = '*';
-            field2 [i][j] = '*';
+            field1 [0][j] = char (j + DIFF);
+            field2 [0][j] = char (j + DIFF);
+            field1 [i][j] = '~';
+            field2 [i][j] = '~';
         }
     }
 
     display (field1);
 
-    int countShip = 4;
-    int desk = 2;
-
-
-    setupShip (field1, desk);
-    //setuoFlot
-
-    /*
-    std::cout <<  "Entering " << countShip << " " <<  desk <<  "-desk ships.\n";
-    setupShip (field1, desk);
-    countShip--;
-    desk++;
-
-    std::cout <<  "Entering " << countShip << " " <<  desk <<  "-desk ships.\n";
-    setupShip (field1, desk);
-    countShip--;
-    desk++;
-     */
-
+    //setup flot
+    for (int desk = 1, countShip = 4; desk <= MAXDESK; desk++)
+    {
+        setupShip(field1, desk, countShip);
+        countShip--;
+    }
     return 0;
 }

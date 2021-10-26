aaa934a2d39e3a747a9185be6b009d7ee34b2f78
Orphan72 <sdenko17@gmail.com>
2021-10-26 09:21:02 +0300

    Task 2 update

--- Block16.4/Task2 Fractional stitcher/main.cpp
+++ Block16.4/Task2 Fractional stitcher/main.cpp
@@ -6,22 +6,38 @@ int main()
     const int DEVIDER = 10;
     int integer    = 0;
     int fractional = 0;
-    int count      = 0;
-    double result  = 0.0;
+
     std::cout << "Enter the integer part of number\n=> ";
     std::cin >> integer;
     std::cout << "Enter the fractional part of a number\n=> ";
-    std::cin >> fractional;
+    do
+    {
+        std::cin >> fractional;
+        if (fractional < 0)
+        {
+            std::cout << "Fractional part of a number must be positive" << std::endl;
+            std::cout << "Try again" << std::endl;
+        }
+    } while (fractional < 0);
+
     std::string str = std::to_string(fractional);
 
     double frac = fractional;
     for (int i = 0; i < str.length(); i++)
     {
        frac = frac/DEVIDER;
-       count++;
     }
 
-    result = integer + frac;
+    double result = 0.0;
+    if (integer < 0)
+    {
+        integer = -integer;
+        result = integer + frac;
+        result = -result;
+    }
+    else
+        result = integer + frac;
+
     std::cout << "result is " << result << std::endl;
 
     return 0;

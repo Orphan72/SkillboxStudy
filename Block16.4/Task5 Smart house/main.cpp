aef15b599a1244302d44337c707a00f38e9cba1b
Orphan72 <sdenko17@gmail.com>
2021-11-03 09:33:48 +0200

    Task 5Smart Hous start

---
+++ Block16.4/Task5 Smart house/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int main()
+{
+    enum switches
+    {
+        POWER,
+        SOCKETS,
+        INSIDELIGHT,
+        OUTSIDELIGHT,
+        INSIDEHEATING,
+        OUTSIDEHEATING,
+        AIRCONDITION
+    };
+
+    enum light
+    {
+        NIGHT,
+        DAY
+    };
+
+    enum move
+    {
+        OFF,
+        ON
+    };
+
+
+    int insideTemperature = 0;
+    int outsideTemperature = 0;
+
+    return 0;
+}
+
+

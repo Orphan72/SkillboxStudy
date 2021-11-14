99015c964e955526c1341f62308430ee41721bb6
Orphan72 <sdenko17@gmail.com>
2021-11-14 17:46:33 +0200

    Task 5 finish

--- Block16.4/Task5 Smart house/main.cpp
+++ Block16.4/Task5 Smart house/main.cpp
@@ -1,163 +1,156 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 
 enum switches
 {
     POWER           = 2,
     SOCKETS         = 4,
-    INSIDE_LIGHT    = 8,
+    INSIDE_ILLUM    = 8,
     OUTSIDE_LIGHT   = 16,
     INSIDE_HEATING  = 32,
     OUTSIDE_HEATING = 64,
     AIRCONDITION    = 128
 };
 
-enum hours
-{
-    ONE = 1,
-    TWO,
-    THREE,
-    FOUR,
-    FIVE,
-    SIX,
-    SEVEN,
-    EIGHT,
-    NINE,
-    TEN,
-    ELEVEN,
-    TWELVE
-};
-
-enum move
-{
-    OFF,
-    ON
-};
-
 enum sunlight
 {
-    NIGHT,
-    DAY
+    LIGHT,
+    TWILIGHT,
+    DARK
 };
 
+const int DAYS = 2;
+const int DAYHOURS = 24;
+const int MORNING = 5;
+const int EVENING = 16;
+const int NIGHT = 20;
+
+const int WIN_TEMP = 0;
+const int SUM_TEMP = 5;
+const int COLD_TEMP = 22;
+const int WARM_TEMP = 25;
+const int HOT_TEMP = 30;
+
+const int COLOR_TEMP_MIN = 2700;
+const int COLOR_TEMP_MAX = 5000;
 
 int main()
 {
-    const int DAYTIME = 24;
-    const int MORNING = 5;
-    const int EVENING = 16;
-    const int WINTER = 0;
-    const int SUMMER = 5;
-
     int state = 0;
-    int outsideTemperature = WINTER;
-    int insideTemperature = 0;
-    int time = 0;
+    int outTemp = WIN_TEMP;
+    int inTemp = 0;
+    int level_illum = 0;
+    bool move_sensor = false;
 
+    std::string input_str;
+    std::string str_move_sensor;
+    std::string str_inside_illum;
 
-    sunlight light = NIGHT;
-    move mv = OFF;
+    sunlight outside_light;
 
+    int diff = NIGHT - EVENING;
+    int color_temp [diff];
+    int step_color = (COLOR_TEMP_MAX - COLOR_TEMP_MIN) / diff;
 
-
-
-
-
-    if (time > MORNING || time < EVENING) light = DAY;
-    else light = NIGHT;
-
-
-    if (outsideTemperature < WINTER)
+    for (int k = 0; k <= diff; k++)
     {
-        state |= OUTSIDE_HEATING;
-    }
-    else if (insideTemperature > SUMMER)
-    {
-        state &= ~OUTSIDE_HEATING;
+        color_temp [k] = COLOR_TEMP_MAX - step_color*k;
     }
 
-    if (light == NIGHT)
+    for (int i = 0; i < DAYS; i++)
     {
-        if (mv == ON)
+        for (int j = 0; j < DAYHOURS; j++)
         {
-            // свет включен
-        }
-        else
-        {
-            // свет выключен
-        }
+            std::stringstream text;
+            int time = j;
+            std::cout << "======================\n";
+            std::cout << "Time is " << time << ":00" << std::endl;
+
+            state |= POWER;
+            state |= SOCKETS;
+
+            std::cout << "Enter:\ntemperature outside,\ntemperature inside,\n";
+            std::cout << "move outside status (Y/N),\nlight inside status (On/Off)\n=>";
+
+            getline(std::cin, input_str);
+            text << input_str;
+            text >> outTemp >> inTemp >> str_move_sensor >> str_inside_illum;
+
+            if (time >= MORNING && time < EVENING)
+                outside_light = LIGHT;
+            else if (time >= EVENING && time < NIGHT)
+                outside_light = TWILIGHT;
+            else
+                outside_light = DARK;
+
+            if (state & POWER)
+            {
+                if (str_move_sensor == "Y")
+                    move_sensor = true;
+                else if (str_move_sensor == "N")
+                    move_sensor = false;
+
+                if (str_inside_illum == "On")
+                    state |= INSIDE_ILLUM;
+                else if (str_inside_illum == "Off")
+                    state &= ~INSIDE_ILLUM;
+
+                if (state & INSIDE_ILLUM)
+                {
+                    if (time >= 0)
+                        level_illum = COLOR_TEMP_MAX;
+                    if (time > EVENING && time <= NIGHT)
+                        level_illum = color_temp[time - EVENING];
+                    else if (time > NIGHT)
+                        level_illum = COLOR_TEMP_MIN;
+                }
+                else
+                    level_illum = 0;
+
+                if ((outside_light == DARK || outside_light == TWILIGHT) && move_sensor)
+                    state |= OUTSIDE_LIGHT;
+                else
+                    state &= ~OUTSIDE_LIGHT;
+
+                if (outTemp < WIN_TEMP)
+                    state |= OUTSIDE_HEATING;
+                else if (outTemp > SUM_TEMP)
+                    state &= ~OUTSIDE_HEATING;
+
+                if (state & SOCKETS)
+                {
+                    if (inTemp < COLD_TEMP)
+                        state |= INSIDE_HEATING;
+                    else if (inTemp >= WARM_TEMP)
+                        state &= ~INSIDE_HEATING;
+                    if (inTemp >= HOT_TEMP)
+                        state |= AIRCONDITION;
+                    else if (inTemp <= WARM_TEMP)
+                        state &= ~AIRCONDITION;
+                }
+                else
+                {
+                    state &= ~INSIDE_HEATING;
+                    state &= ~AIRCONDITION;
+                }
+            }
+            else
+            {
+                state &= ~OUTSIDE_HEATING;
+                state &= ~INSIDE_HEATING;
+                state &= ~AIRCONDITION;
+                state &= ~OUTSIDE_LIGHT;
+                state &= ~INSIDE_ILLUM;
+            }
+
+            std::cout << ((state & OUTSIDE_HEATING) ? "OUTSIDE HEATING is ON " : "OUTSIDE HEATING is OFF ") << std::endl;
+            std::cout << ((state & INSIDE_HEATING) ? "INSIDE HEATING is ON " : "INSIDE HEATING is OFF ") << std::endl;
+            std::cout << ((state & AIRCONDITION) ? "AIRCONDITION is ON " : "AIRCONDITION is OFF ") << std::endl;
+            std::cout << ((state & OUTSIDE_LIGHT) ? "OUTSIDE ILLUMINATIONT is ON " : "OUTSIDE LIGHT is OFF ") << std::endl;
+            std::cout << "INSIDE ILLUMINATION LEVEL is " << level_illum << std::endl;
+           }
     }
-    else
-    {
-        // свет выключен
-    }
-
-
-
-
-
-
-
-
-    state |= SOCKETS;
-
-    if (state & POWER) std::cout << "Power of hous ON";
-    if (state & SOCKETS) std::cout << "Socket ON";
-    if (state & INSIDE_LIGHT) std::cout << "Inside light ON";
-    if (state & OUTSIDE_LIGHT) std::cout << "Outside light ON";
-    if (state & INSIDE_HEATING) std::cout << "Inside heating ON";
-    if (state & OUTSIDE_HEATING) std::cout << "Outside heating ON";
-    if (state & AIRCONDITION) std::cout << "Aircondition ON";
-
-
-    state &= ~SOCKETS;
-
-
-    if (state | POWER) std::cout << "Power of hous OFF";
-    if (state | SOCKETS) std::cout << "Socket OFF";
-    if (state | INSIDE_LIGHT) std::cout << "Inside light OFF";
-    if (state | OUTSIDE_LIGHT) std::cout << "Outside light OFF";
-    if (state | INSIDE_HEATING) std::cout << "Inside heating OFF";
-    if (state | OUTSIDE_HEATING) std::cout << "Outside heating OFF";
-    if (state | AIRCONDITION) std::cout << "Aircondition OFF";
-
-
-
-
-
-
-
-
-
-
 
     return 0;
 }
-/*
-
-enum time
-{
-    ONE,
-    TWO,
-    THREE,
-    FOUR,
-    FIVE,
-    SIX,
-    SEVEN,
-    EIGHT,
-    NINE,
-    TEN,
-    ELEVEN,
-    TWELVE
-};
-
- int hours [DAYTIME];
-    for (int i = 1; i <= DAYTIME; i++) hours [i] = i;
-
-
-
-
-
-*/
-
-

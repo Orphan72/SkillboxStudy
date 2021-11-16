facfe09d04b3b3f5cb7a776221aada9a1d8c98b8
Orphan72 <sdenko17@gmail.com>
2021-11-16 09:07:54 +0200

    Task 1 Bl 17 finish

--- Block17.4/Task1/main.cpp
+++ Block17.4/Task1/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
+void swap (int *x, int* y)
+{
+    *x += *y;
+    *y = *x - *y;
+    *x -=*y;
+}
+
+
 int main()
 {
+    int a = 10;
+    int b = 20;
+
+    swap (&a, &b);
+
+    std::cout << a << " " << b << std::endl;
+
     return 0;
 }

9ce4c101a731af83560b5e9c59ff89250f7a3baf
Orphan72 <sdenko17@gmail.com>
2021-11-18 09:34:00 +0200

    Task 2 temp commit

--- Block16.4/Task2 Fractional stitcher/main.cpp
+++ Block16.4/Task2 Fractional stitcher/main.cpp
@@ -3,23 +3,38 @@
 
 int main()
 {
-    const int DEVIDER = 10;
-    int integer    = 0;
-    std::string str_frac = " ";
+    //const int DEVIDER = 10;
+    ///int integer    = 0;
+    std::string str_int {};
+    std::string str_frac {};
+    std::string str_dot = ".";
+    std::string str_double {};
+
+
 
     std::cout << "Enter the integer part of number\n=> ";
-    std::cin >> integer;
+    //std::cin >> str_int;
+    str_int = "12345";
     std::cout << "Enter the fractional part of a number\n=> ";
-    std::cin >> str_frac;
-    double num_frac = std::stoi(str_frac);
+    str_frac = "5678";
+    //std::cin >> str_frac;
+
+    str_double = str_int + str_dot;
+    str_double = str_double + str_frac;
+
+
+    double num_frac = std::stoi(str_double);
+
+
+
+
+
+
     std::cout << "num_frac " << num_frac << std::endl;
-    for (int i = 0; i < str_frac.length(); i++)
-    {
-       num_frac /= DEVIDER;
-    }
 
-    double result = (integer < 0) ? integer - num_frac : integer + num_frac;
 
-    std::cout << "result is " << result << std::endl;
+    //double result = (integer < 0) ? integer - num_frac : integer + num_frac;
+
+    //std::cout << "result is " << result << std::endl;
     return 0;
 }

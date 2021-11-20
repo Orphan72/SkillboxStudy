4b11799a16ced88ab561057331772044e1aa614c
Orphan72 <sdenko17@gmail.com>
2021-11-20 18:31:41 +0200

    Task 2 Block 16 update

--- Block16.4/Task2 Fractional stitcher/main.cpp
+++ Block16.4/Task2 Fractional stitcher/main.cpp
@@ -1,40 +1,25 @@
 #include <iostream>
 #include <string>
+#include <iomanip>
 
 int main()
 {
-    //const int DEVIDER = 10;
-    ///int integer    = 0;
     std::string str_int {};
     std::string str_frac {};
     std::string str_dot = ".";
     std::string str_double {};
 
-
-
     std::cout << "Enter the integer part of number\n=> ";
-    //std::cin >> str_int;
-    str_int = "12345";
+    std::cin >> str_int;
     std::cout << "Enter the fractional part of a number\n=> ";
-    str_frac = "5678";
-    //std::cin >> str_frac;
+    std::cin >> str_frac;
 
     str_double = str_int + str_dot;
     str_double = str_double + str_frac;
 
+    unsigned int leight = str_double.length();
+    double num_frac = std::stod(str_double);
 
-    double num_frac = std::stoi(str_double);
-
-
-
-
-
-
-    std::cout << "num_frac " << num_frac << std::endl;
-
-
-    //double result = (integer < 0) ? integer - num_frac : integer + num_frac;
-
-    //std::cout << "result is " << result << std::endl;
+    std::cout << "result is " << std::setprecision(leight) << num_frac << std::endl;
     return 0;
 }

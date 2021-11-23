716d4d18399f548322139875fa640c7b497f7e6b
Orphan72 <sdenko17@gmail.com>
2021-11-23 09:32:35 +0200

    Finish task1

--- Block18.5/Task1/main.cpp
+++ Block18.5/Task1/main.cpp
@@ -1,7 +1,53 @@
 #include <iostream>
+#include <vector>
+
+int const SIZE = 5;
+
+void swapvec (std::vector<int> &vc, int* ar)
+{
+    for (int i = 0; i < SIZE; i++)
+    {
+        ar [i] += vc [i];
+        vc [i] = ar [i] - vc [i];
+        ar [i] -= vc [i];
+    }
+}
+
+void showVec (std::vector<int> vc)
+{
+    for (int i = 0; i < SIZE; i++)
+    {
+        std::cout << vc [i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+void showArr (int *arr)
+{
+    for (int i = 0; i < SIZE; i++)
+    {
+        std::cout << arr [i] << " ";
+    }
+    std::cout << std::endl;
+}
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    std::vector<int> vec = {1,2,3,4,5};
+    int arr [] = {6,7,8,9,10};
+
+    std::cout << "Origin: \n";
+
+    showVec (vec);
+    showArr (arr);
+
+    swapvec (vec, arr);
+
+    std::cout << "After swap: \n";
+
+    showVec (vec);
+    showArr (arr);
+
     return 0;
 }
+

7cf639c2452545b44a55bbb9cbeec8b0c9487c6c
Orphan72 <sdenko17@gmail.com>
2021-12-01 12:24:01 +0200

    Task 4 Block 18 finish

--- Block18.5/Task4/main.cpp
+++ Block18.5/Task4/main.cpp
@@ -1,76 +1,22 @@
 #include <iostream>
 
-int count = 0;
-
-int foo (int num)
+void evendigits (long long nm, int &ans)
 {
-    //int count = 0;
+    if (nm%2 == 0)
+        ans++;
 
-    if (num == 0)
-        return count;
+    nm /= 10;
 
-    if (num%2 == 0)
-        count++;
-
-    return foo (num/10);
-
-
+    if (nm != 0)
+        evendigits (nm, ans);
 }
-
 
 int main()
 {
-
-    long num = 45602;
-    int a = 0;
-
-    a = foo (45632);
-
-    std::cout << a << std::endl;
+    long long num = 9223372036854775806;
+    int answer = 0;
+    evendigits (num, answer);
+    std::cout <<answer << std::endl;
 
     return 0;
 }
-
-
-
-
-/*
-int main()
-{
-
-    long num = 45602;
-    int count = 0;
-
-    for (int i = 0; i < 5; i++)
-    {
-
-        if (num % 2 == 0)
-        {
-            count++;
-
-        }
-        num = num / 10;
-        std::cout << count << std::endl;
-        std::cout << num << std::endl;
-    }
-    std::cout << count << std::endl;
-    return 0;
-}
-*/
-
-
-
-/*
-Напишите рекурсивную функцию, которая принимает большое число n типа long long и переменную ans, а возвращает void.
-После завершения работы функции в переменной ans должно оказаться количество чётных цифр в записи числа n.
-
-Пример:
-
-int ans;
-
-evendigits(9 223 372 036 854 775 806, ans);
-
-//ans == 10
-
-Пояснение: 9 223 372 036 854 775 806
-*/

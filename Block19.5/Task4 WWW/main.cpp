91996106794ec08866ac9a4d3583ab4e179ae4c2
Orphan72 <sdenko17@gmail.com>
2021-12-26 21:18:33 +0200

    Task 5 WWW contin_2

--- Block19.5/Task4 WWW/main.cpp
+++ Block19.5/Task4 WWW/main.cpp
@@ -42,7 +42,7 @@ void show (std::string pth, char* buffer, qustAnsw qa, bool &isOpn)
         {
             std::cout << "The correct answer is - ";
         }
-        std::cout << "===================\n";
+        std::cout << "\n===================\n";
 
         while (!text.eof())
         {
@@ -58,6 +58,9 @@ void show (std::string pth, char* buffer, qustAnsw qa, bool &isOpn)
     }
     std::cout << "\n===================\n";
 }
+
+
+
 
 
 int main()
@@ -76,12 +79,17 @@ int main()
     int sector = 0;
     bool isFileOpened = true;
 
+    int countViewers = 0;
+    int countExperts = 0;
+
     rotation(sector);
     std::cout << "Sector is  " << sector << std::endl;
 
     pathQues = pathQuesBegin + sectChar [sector -1] + pathEnd;
+    pathAnsw = pathAnswBegin + sectChar [sector -1] + pathEnd;
 
-    std::cout << "Path is  " << pathQues << std::endl;
+    std::cout << "Path 1 is  " << pathQues << std::endl;
+    std::cout << "Path 2 is  " << pathAnsw << std::endl;
 
     show (pathQues, question, QUES, isFileOpened);
 
@@ -94,12 +102,42 @@ int main()
         std::cout << "Enter your answer:\n";
         std::cin >> answer;
         std::cout << "Answer " << answer << std::endl;
+        show (pathAnsw, correctAnswer, ANSW, isFileOpened);
+        if (!isFileOpened)
+        {
+            std::cout << "The file was not opened!!!\n";
+        }
+        else
+        {
+            //to separate funtion
+            //очки передать по ссылке
 
+            if (correctAnswer == answer)
+            {
+                std::cout << "Correct\n";
+                std::cout << "The point is awarded to experts\n";
+                countExperts++;
+            }
+            else
+            {
+                std::cout << "Nooo\n";
+                std::cout << "The point is awarded to viewers\n";
+                countViewers++;
+            }
+
+            std::cout << "The Score is";
+            std::cout << "\n====================\n";
+            std::cout << countViewers << " : " <<  countExperts;
+            std::cout << " in favor of ";
+            std::cout << ( countViewers > countExperts ? "Viewers" : "Experts");
+            std::cout << "\n====================\n";
+
+            //====
     }
 
 
 
-    //}
+    }
 
 
 
@@ -117,18 +155,13 @@ int main()
         относительно текущего сектора на барабане. Исходя из этого офсета вычисляется новый активный сектор,
         который и будет играть в этом ходе. Если выпавший сектор уже играл, выбирает следующий неигравший за ним.
         В начале всей игры стрелка установлена на первом секторе.
-
 Как только играет какой-то из секторов, с него берётся письмо с вопросом — то есть считывается из файла
 данного сектора. Вопрос показывается на экране.
-
 После того как вопрос показан, от игрока ожидается ответ на него.
 Игрок вводит этот ответ с помощью стандартного ввода. То, что он ввёл, сравнивается с ответом,
 который хранится во втором файле, ассоциированном с активным сектором.
 Данный файл должен содержать лишь одно слово-ответ.
-
 Если ответ знатока-игрока был правильным, ему начисляется один балл. Если неверен, то балл уходит телезрителям.
-
 Игра продолжается до тех пор, пока или игрок, или зрители не наберут шесть баллов.
 После этого называется победитель и программа заканчивает работу.
-
  */

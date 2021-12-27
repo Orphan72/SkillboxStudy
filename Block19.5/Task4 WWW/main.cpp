e542b6f1412d7e3d1ce98bee8e5f7692ae33382f
Orphan72 <sdenko17@gmail.com>
2021-12-27 18:30:26 +0200

    Task 5 WWW finish

--- Block19.5/Task4 WWW/main.cpp
+++ Block19.5/Task4 WWW/main.cpp
@@ -2,8 +2,9 @@
 #include <fstream>
 #include <vector>
 
-int const NUM = 13;
-int const SIZE = 20;
+const int NUMSECT = 13;
+const int BUFSIZE = 20;
+const int MAXPOINT = 6;
 
 enum qustAnsw
 {
@@ -11,16 +12,14 @@ enum qustAnsw
     ANSW
 };
 
-
 void rotation (int &sec)
 {
     int offset = 0;
-
     std::cout << "Please, spint the dream" << std::endl;
     std::cout << "Enter offset\n=>";
     std::cin >> offset;
     sec += offset;
-    while (sec > NUM) sec -= NUM;
+    while (sec > NUMSECT) sec -= NUMSECT;
 }
 
 void show (std::string pth, char* buffer, qustAnsw qa, bool &isOpn)
@@ -36,19 +35,19 @@ void show (std::string pth, char* buffer, qustAnsw qa, bool &isOpn)
     {
         if (qa == QUES)
         {
-            std::cout << "Attention! The question:\n";
+            std::cout << "Attention! The question is:";
         }
         else
         {
-            std::cout << "The correct answer is - ";
+            std::cout << "The correct answer is:";
         }
         std::cout << "\n===================\n";
 
         while (!text.eof())
         {
-            text.read(buffer, SIZE - 1);
+            text.read(buffer, BUFSIZE - 1);
 
-            if (text.gcount() < SIZE)
+            if (text.gcount() < BUFSIZE)
             {
                 int count = text.gcount();
                 buffer[count] = 0;
@@ -57,15 +56,54 @@ void show (std::string pth, char* buffer, qustAnsw qa, bool &isOpn)
         }
     }
     std::cout << "\n===================\n";
+    text.close();
 }
 
+void showConclusion (const char* corAnsw, std::string answ, int& cExp, int& cView )
+{
+        if (corAnsw == answ)
+        {
+            std::cout << "Correct\n";
+            std::cout << "The point is awarded to experts\n";
+            cExp++;
 
+        }
+        else
+        {
+            std::cout << "Nooo\n";
+            std::cout << "The point is awarded to viewers\n";
+            cView++;
+        }
 
+        std::cout << "The Score is";
+        std::cout << "\n====================\n";
+        std::cout << cView << " : " <<  cExp;
+        std::cout << " in favor of ";
+        std::cout << ( cView > cExp ? "Viewers" : "Experts");
+        std::cout << "\n====================\n";
+};
 
+void checksector (std::vector<int> scts, int& sec)
+{
+    for (int i = 0; i < scts.size(); i++)
+    {
+        if (sec == scts [i])
+        {
+            std::cout << "Sector " << sec << " already used\n";
+            std::cout << "Will be choosed next one\n";
+            sec++;
+            while (sec > NUMSECT) sec -= NUMSECT;
+            checksector (scts, sec);
+         }
+    }
+}
 
 int main()
 {
-    std::vector <std::string> sectChar = {"051","02","03","04","05","06","07","08","09","10","11","12","13"};
+    std::vector <std::string> sectors = {"01","02","03","04","05","06","07","08","09","10","11","12","13"};
+    int sector = 0;
+    std::vector <int> usedSectors;
+
     std::string pathQuesBegin = "..//data//questions//Q";
     std::string pathAnswBegin = "..//data//answers//A";
     std::string pathEnd = ".txt";
@@ -73,95 +111,52 @@ int main()
     std::string pathAnsw = " ";
 
     std::string answer = " ";
-    char question [SIZE];
-    char correctAnswer [SIZE];
+    char question [BUFSIZE];
+    char correctAnswer [BUFSIZE];
 
-    int sector = 0;
     bool isFileOpened = true;
 
     int countViewers = 0;
     int countExperts = 0;
 
-    rotation(sector);
-    std::cout << "Sector is  " << sector << std::endl;
-
-    pathQues = pathQuesBegin + sectChar [sector -1] + pathEnd;
-    pathAnsw = pathAnswBegin + sectChar [sector -1] + pathEnd;
-
-    std::cout << "Path 1 is  " << pathQues << std::endl;
-    std::cout << "Path 2 is  " << pathAnsw << std::endl;
-
-    show (pathQues, question, QUES, isFileOpened);
-
-    if (!isFileOpened)
+    while (countViewers < MAXPOINT && countExperts < MAXPOINT)
     {
-        std::cout << "The file was not opened!!!\n";
-    }
-    else
-    {
-        std::cout << "Enter your answer:\n";
-        std::cin >> answer;
-        std::cout << "Answer " << answer << std::endl;
-        show (pathAnsw, correctAnswer, ANSW, isFileOpened);
+        rotation(sector);
+
+        checksector (usedSectors, sector);
+        std::cout << "Sector " << sector << std::endl;
+        usedSectors.push_back(sector);
+
+        pathQues = pathQuesBegin + sectors [sector - 1] + pathEnd;
+        pathAnsw = pathAnswBegin + sectors [sector - 1] + pathEnd;
+
+        show(pathQues, question, QUES, isFileOpened);
+
         if (!isFileOpened)
         {
             std::cout << "The file was not opened!!!\n";
         }
         else
         {
-            //to separate funtion
-            //очки передать по ссылке
+            std::cout << "Enter your answer:\n";
+            std::cin >> answer;
+            std::cout << "Answer " << answer << std::endl;
+            show(pathAnsw, correctAnswer, ANSW, isFileOpened);
 
-            if (correctAnswer == answer)
+            if (!isFileOpened)
             {
-                std::cout << "Correct\n";
-                std::cout << "The point is awarded to experts\n";
-                countExperts++;
+                std::cout << "The file was not opened!!!\n";
             }
             else
             {
-                std::cout << "Nooo\n";
-                std::cout << "The point is awarded to viewers\n";
-                countViewers++;
+                showConclusion(correctAnswer, answer, countExperts, countViewers);
             }
-
-            std::cout << "The Score is";
-            std::cout << "\n====================\n";
-            std::cout << countViewers << " : " <<  countExperts;
-            std::cout << " in favor of ";
-            std::cout << ( countViewers > countExperts ? "Viewers" : "Experts");
-            std::cout << "\n====================\n";
-
-            //====
+        }
     }
 
-
-
-    }
-
-
-
-
-
-
+    std::cout << ( countViewers > countExperts ? "Viewers " : "Experts ");
+    std::cout << " WON!!\n";
 
     return 0;
 }
 
-/*
-Как происходит игра
-        В начале каждого хода игроки вращают волчок, то есть выбирают сектор на столе, который сейчас играет.
-        Всего таких секторов 13. Сектор выбирается так: с клавиатуры вводится офсет (смещение)
-        относительно текущего сектора на барабане. Исходя из этого офсета вычисляется новый активный сектор,
-        который и будет играть в этом ходе. Если выпавший сектор уже играл, выбирает следующий неигравший за ним.
-        В начале всей игры стрелка установлена на первом секторе.
-Как только играет какой-то из секторов, с него берётся письмо с вопросом — то есть считывается из файла
-данного сектора. Вопрос показывается на экране.
-После того как вопрос показан, от игрока ожидается ответ на него.
-Игрок вводит этот ответ с помощью стандартного ввода. То, что он ввёл, сравнивается с ответом,
-который хранится во втором файле, ассоциированном с активным сектором.
-Данный файл должен содержать лишь одно слово-ответ.
-Если ответ знатока-игрока был правильным, ему начисляется один балл. Если неверен, то балл уходит телезрителям.
-Игра продолжается до тех пор, пока или игрок, или зрители не наберут шесть баллов.
-После этого называется победитель и программа заканчивает работу.
- */

b53b693625803ec3cacdb3b06068d14aeff7a262
Orphan72 <sdenko17@gmail.com>
2021-12-29 11:20:47 +0200

    Block 20

--- TEST/main.cpp
+++ TEST/main.cpp
@@ -1,47 +1,60 @@
 #include <iostream>
-#include <string>
+#include <cstring>
 #include <fstream>
 #include <vector>
+#include <cstdlib>
 
 
 int main()
 {
+    int control_sum = 0;
+    std::vector <int> buildings;
+    int count = std::rand () %901 + 100;
+    for (int i = 0; i < count; i++)
+    {
+        buildings.push_back(std::rand () %51);
+        control_sum += buildings [i];
+    }
 
-  std::vector<double> purse;
-  std::ifstream bank;
-  bank.open ("E:\\My documents\\SkillBox\\bank.txt");
+    std::cout << "Control_sum is "<< control_sum << std::endl;
+    std::ofstream file ("..\\data\\buildings.txt");
+    file << count << std::endl;
 
-  double sum = 0;
-  while (!bank.eof())
-  {
-      double money;
-      bank >> money;
-      purse.push_back(money);
-  }
+    for (int i = 0; i < count; i++)
+    {
+        file << buildings [i] << std::endl;
+    }
+    file << "SkillCity" << std::endl;
+
+    file.close();
+
+        std::ifstream in_file ("..\\data\\buildings.txt");
+        std::vector <int> in_buildings;
+        int in_count;
+        int in_sum = 0;
+        in_file >> in_count;
+
+        for (int i = 0; i < in_count; i++)
+        {
+            int in_people;
+            in_file >> in_people;
+            in_buildings.push_back(in_people);
+        }
+
+        std::string in_city;
+        in_file >> in_city;
+
+
+        for (int i = 0; i < in_count; i++)
+        {
+            in_sum += in_buildings [i];
+        }
 
 
 
-  for (int i = 0; i < purse.size(); ++i)
-  {
-      sum += purse [i];
-  }
-
-  std::cout << sum << std::endl;
-
-    bank.close();
+        std::cout << "Population of " << in_city << " is "<< in_sum << std::endl;
 
 
 
 }
 
-
-
-
-/*
-
-evendigits(9 223 372 036 854 775 806, ans);
-
-//ans == 10
-
-Пояснение: 9 223 372 036 854 775 806
-*/

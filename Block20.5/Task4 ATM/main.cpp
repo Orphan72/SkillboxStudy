35ac507e0971a0994576fb437ccaeebf9ce9e958
Orphan72 <sdenko17@gmail.com>
2022-01-17 17:41:50 +0200

    Task4 ATM mediate4

--- Block20.5/Task4 ATM/main.cpp
+++ Block20.5/Task4 ATM/main.cpp
@@ -3,110 +3,155 @@
 #include <fstream>
 
 int SIZE = 6;
-int MAXCOUNT = 10;
+int MAXCOUNTBANKN = 1000;
 const int BANKNOTES [] = {5000, 2000, 1000, 500, 200, 100};
 
 int getavailableSum (int *bxs)
 {
-    int suum = 0;
+    int sum = 0;
     for (int i = 0; i < SIZE; i++)
     {
-        suum += (bxs [i] * BANKNOTES [i]);
+        sum += (bxs [i] * BANKNOTES [i]);
     }
+    return sum;
+}
 
-    return suum;
+int getCountBankn (int *bxs)
+{
+    int bnk = 0;
+    for (int i = 0; i < SIZE; i++)
+    {
+        bnk += bxs [i];
+    }
+    return bnk;
 }
 
 int main()
 {
 
     std::srand(std::time(nullptr));
-
     int boxes [] = {0, 0, 0, 0, 0, 0};
     int availableSum = 0;
-    int wishSum = 2000;
+    int wishSum = 0;
     int bank = 0;
+    int countBank = 0;
+    char answer = ' ';
+
+
+    bool correctAnswer = false;
 
 
     std::ifstream memoryFrom ("..\\data\\memory.bin", std::ios::binary);
+
 
     if (memoryFrom.is_open())
     {
         std::cout << "=memory open=\n";
         memoryFrom.read((char *) boxes, sizeof(boxes));
-
-
-
-        /*
-        for (int i = 0; i < MAXCOUNT; i++)
-        {
-            int num = std::rand()%SIZE;
-            std::cout << BANKNOTES [num] << std::endl;
-            boxes [num]++;
-        }
-         */
-
         availableSum = getavailableSum(boxes);
         std::cout << "rest in ATM  "<< availableSum << std::endl;
 
-        for (int i = 0; i < SIZE; i++)
+        countBank = getCountBankn (boxes);
+        std::cout << "banknotes in ATM  "<< countBank << std::endl;
+
+        std::cout  << "if you want to full ATM, please, press \"+\"\n";
+        std::cout  << "if you want to get monery, please, press \"-\"\n=>";
+
+        while (!correctAnswer)
         {
-            std::cout << boxes[i] << " ";
+            std::cin >> answer;
+            correctAnswer = (answer == '+' || answer == '-');
+            if (!correctAnswer)
+                std::cout << "Answer incorrect, please, try again\n";
         }
 
-        std::cout << std::endl;
-
-
-        std::cout << "Wish sum " << wishSum << std::endl;
-
-        if (wishSum > availableSum)
+        if (answer == '+')
         {
-            std::cout << "Not enough money in the ATM ";
+            for (int i = 0; i < (MAXCOUNTBANKN - countBank); i++)
+            {
+                int num = std::rand()%SIZE;
+                boxes [num]++;
+            }
+
+            availableSum = getavailableSum(boxes);
+            std::cout << "rest in ATM  "<< availableSum << std::endl;
+
+            countBank = getCountBankn (boxes);
+            std::cout << "banknotes in ATM  "<< countBank << std::endl;
         }
         else
         {
-            std::cout << "Get your money:\n";
+            availableSum = getavailableSum(boxes);
+            std::cout << "rest in ATM  "<< availableSum << std::endl;
 
-
-            for (int box = 0; box < SIZE; box++)
-            {
-                if (boxes[box] == 0)
-                {
-                    box++;
-                }
-
-                //std::cout << "box - " << box << std::endl;
-                bank = *(BANKNOTES + box);
-                while (wishSum >= bank)
-                {
-                    if (boxes[box] == 0) break;
-                    std::cout << "banknot  " << bank << std::endl;
-
-                    wishSum -= bank;
-                    boxes[box]--;
-                    std::cout << "sum in while " << wishSum << std::endl;
-                    //std::cout << "boxes [box] " << boxes [box] << std::e
-                }
-            }
-
-            std::cout << "final sum " << wishSum << std::endl;
-            if (wishSum > 0)
-            {
-                std::cout << "it is impossible to provide this sum " << std::endl;
-            }
+            countBank = getCountBankn (boxes);
+            std::cout << "banknotes in ATM  "<< countBank << std::endl;
 
             for (int i = 0; i < SIZE; i++)
             {
                 std::cout << boxes[i] << " ";
             }
+
+            std::cout << std::endl;
+
+            std::cout << "Enter wish sum\n=>";
+            std::cin >> wishSum;
+            if (wishSum > availableSum)
+            {
+                std::cout << "Not enough money in the ATM ";
+            }
+            else
+            {
+                std::ofstream banknotes ("..\\data\\banknotes.bin", std::ios::binary);
+
+                std::cout << "Get your money:\n";
+                for (int box = 0; box < SIZE; box++)
+                {
+                    if (boxes[box] == 0)
+                    {
+                        box++;
+                    }
+                    bank = *(BANKNOTES + box);
+                    while (wishSum >= bank)
+                    {
+                        if (boxes[box] == 0) break;
+                        std::cout << "banknot  " << bank << std::endl;
+
+                        wishSum -= bank;
+                        boxes[box]--;
+
+                        std::cout << "wishsum  " << wishSum << std::endl;
+
+                    }
+                }
+
+                if (wishSum > 0)
+                {
+
+                    memoryFrom.close();
+                    std::cout << "it is impossible to provide this sum " << std::endl;
+
+                    std::ifstream memFrom ("..\\data\\memory.bin", std::ios::binary);
+                    memFrom.read((char *) boxes, sizeof(boxes));
+                    for (int i = 0; i < SIZE; i++)
+                    {
+                        std::cout << boxes[i] << " ";
+                    }
+                    memFrom.close();
+
+                }
+
+                for (int i = 0; i < SIZE; i++)
+                {
+                    std::cout << boxes[i] << " ";
+                }
+            }
         }
 
-
         availableSum = getavailableSum(boxes);
-
-
-
-        std::cout << "rest in ATM  "<< availableSum;
+        countBank = getCountBankn (boxes);
+        std::cout << "rest in ATM  "<< availableSum << std::endl;
+        std::cout << "banknotes in ATM  "<< countBank << std::endl;
     }
     else
     {
@@ -125,20 +170,6 @@ int main()
         std::cout << "=The memory of ATM is not available for writing=\n";
     }
     memoryTo.close();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     return 0;
 }

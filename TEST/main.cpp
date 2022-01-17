7d7d8c9719f17dad5d14c361d841f56d82031a25
Orphan72 <sdenko17@gmail.com>
2022-01-17 14:49:22 +0200

    Block 20

--- TEST/main.cpp
+++ TEST/main.cpp
@@ -1,58 +1,81 @@
 #include <iostream>
-#include <cstring>
 #include <fstream>
-#include <vector>
-#include <cstdlib>
+
 
 
 int main()
 {
-    int control_sum = 0;
-    std::vector <int> buildings;
-    int count = std::rand () %901 + 100;
-    for (int i = 0; i < count; i++)
+
+    int boxes [6];
+
+    std::ifstream memoryFrom ("..\\data\\memory.bin", std::ios::binary);
+    memoryFrom.read((char*)boxes, sizeof (boxes));
+
+
+    //std::cout << sizeof (boxes);
+
+
+    for (int i = 0; i < 6; i++)
     {
-        buildings.push_back(std::rand () %51);
-        control_sum += buildings [i];
+        boxes [i] = 0;
     }
 
-    std::cout << "Control_sum is "<< control_sum << std::endl;
-    std::ofstream file ("..\\data\\buildings.txt");
-    file << count << std::endl;
-
-    for (int i = 0; i < count; i++)
+    for (int i = 0; i < 6; i++)
     {
-        file << buildings [i] << std::endl;
+        std::cout << boxes [i] << " ";
     }
-    file << "SkillCity" << std::endl;
-
-    file.close();
-
-        std::ifstream in_file ("..\\data\\buildings.txt");
-        std::vector <int> in_buildings;
-        int in_count;
-        int in_sum = 0;
-        in_file >> in_count;
-
-        for (int i = 0; i < in_count; i++)
-        {
-            int in_people;
-            in_file >> in_people;
-            in_buildings.push_back(in_people);
-        }
-
-        std::string in_city;
-        in_file >> in_city;
-
-
-        for (int i = 0; i < in_count; i++)
-        {
-            in_sum += in_buildings [i];
-        }
 
 
 
-        std::cout << "Population of " << in_city << " is "<< in_sum << std::endl;
+    memoryFrom.close();
+
+
+
+    std::ofstream memoryTo ("..\\data\\memory.bin", std::ios::binary);
+
+    memoryTo.write((char*)boxes, sizeof (boxes));
+    memoryTo.close();
+
+    for (int i = 0; i < 6; i++)
+    {
+        std::cout << boxes [i] << " ";
+    }
+
+
+
+
+
+
+    /*
+
+
+    for (int i = 0; i < 6 || (!memoryFrom.eof()); i++)
+    {
+        memoryFrom >> boxes [i];
+    }
+
+
+    for (int i = 0; i < 6 || (!memoryFrom.eof()); i++)
+    {
+        std::cout <<  boxes [i] << std:: endl;
+    }
+
+    std::cout <<  "Enter six numbers\n=>";
+
+    for (int i = 0; i < 6 || (!memoryFrom.eof()); i++)
+    {
+        std::cin >> boxes [i];
+    }
+
+    for (int i = 0; i < 6 || (!memoryFrom.eof()); i++)
+    {
+        std::cout <<  boxes [i] << std:: endl;
+    }
+
+*/
+
+
+
 
 
 

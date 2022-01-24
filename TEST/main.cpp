a38c0b0aa63176d51c23f0e874876bcffa056497
Orphan72 <sdenko17@gmail.com>
2022-01-24 09:17:42 +0200

    Test

--- TEST/main.cpp
+++ TEST/main.cpp
@@ -1,84 +1,85 @@
 #include <iostream>
 #include <fstream>
+#include <cstdlib>
+#include <cstring>
 
+
+struct character
+{
+    std::string name = "unknown";
+    int health = 0;
+    int armor = 0;
+};
+
+void take_damage (character& person, int damage)
+{
+    std::cout << person.name << " took damage: - " << damage << std::endl;
+    person.armor -= damage;
+    if (person.armor < 0)
+    {
+        person.health += person.armor;
+        person.armor = 0;
+    }
+}
+
+void save_character (std::ofstream &file, character& person)
+{
+    int len = person.name.length();
+    file.write((char *)& len, sizeof(len));
+    file.write(person.name.c_str(), len);
+    file.write ((char*) &person.health, sizeof (person.health));
+    file.write ((char*) &person.armor, sizeof (person.armor));
+}
+
+void load_character (std::ifstream &file, character& person)
+{
+    int len;
+    file.read ((char*) &len, sizeof (len));
+    person.name.resize(len);
+    file.read((char*)person.name.c_str(), len);
+    file.read ((char*) &person.health, sizeof (person.health));
+    file.read ((char*) &person.armor, sizeof (person.armor));
+}
 
 
 int main()
 {
+    character people [10];
 
-    int boxes [6];
-
-    std::ifstream memoryFrom ("..\\data\\memory.bin", std::ios::binary);
-    if (memoryFrom.is_open())
+    /*for (int i = 0; i < 10; i++)
     {
-        memoryFrom.read((char *) boxes, sizeof(boxes));
+        people[i].name = "Character #" + std::to_string(i);
+        people[i].health = (std::rand() %100 + 100);
+        people[i].armor = (std::rand() %40 + 10);
     }
+     */
 
-    //std::cout << sizeof (boxes);
 
-
-    for (int i = 0; i < 6; i++)
+/*
+    for (int i = 0; i < 10; i++)
     {
-        boxes [i] = 0;
+        take_damage (people [i], 30);
     }
-
-    for (int i = 0; i < 6; i++)
-    {
-        std::cout << boxes [i] << " ";
-    }
-
-
-
-    memoryFrom.close();
-
-
-
-    std::ofstream memoryTo ("..\\data\\memory.bin", std::ios::binary);
-
-    memoryTo.write((char*)boxes, sizeof (boxes));
-    memoryTo.close();
-
-    for (int i = 0; i < 6; i++)
-    {
-        std::cout << boxes [i] << " ";
-    }
-
-
-
-
-
-
-    /*
-
-
-    for (int i = 0; i < 6 || (!memoryFrom.eof()); i++)
-    {
-        memoryFrom >> boxes [i];
-    }
-
-
-    for (int i = 0; i < 6 || (!memoryFrom.eof()); i++)
-    {
-        std::cout <<  boxes [i] << std:: endl;
-    }
-
-    std::cout <<  "Enter six numbers\n=>";
-
-    for (int i = 0; i < 6 || (!memoryFrom.eof()); i++)
-    {
-        std::cin >> boxes [i];
-    }
-
-    for (int i = 0; i < 6 || (!memoryFrom.eof()); i++)
-    {
-        std::cout <<  boxes [i] << std:: endl;
-    }
-
 */
 
 
+   // people[0].name = "Sebile";
+
+    //std::ofstream file ("..//data//save.bin", std::ios::binary);
+    std::ifstream file ("..//data//save.bin", std::ios::binary);
 
 
+
+    for (int i = 0; i < 10; i++)
+    {
+        //save_character (file, people[i]);
+        load_character (file, people[i]);
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << people[i].name << std::endl;
+    }
 
 
 }

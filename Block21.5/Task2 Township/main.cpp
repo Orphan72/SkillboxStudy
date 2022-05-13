f3871f18131842c9c08c26baee5e7749dbd159c7
Orphan72 <sdenko17@gmail.com>
2022-05-13 21:35:19 +0300

    Task 2 street contin10

--- Block21.5/Task2 Township/main.cpp
+++ Block21.5/Task2 Township/main.cpp
@@ -4,9 +4,6 @@
 
 //TODO
 
-// запись в файл?
-// чтение из файла?
-
 // создать функцию для рандомного заполнения
 
 const int MINCOUTSTREET = 1;
@@ -39,8 +36,7 @@ enum streetNames
     UNKNOWNSTREET,
     LESNAYA,
     OZERNAYA,
-    RECNAYA,
-    PYLNAYA
+    RECNAYA
 };
 
 enum buildNames
@@ -97,6 +93,7 @@ struct section
     int number = 0;
     float area = 0.0;
     int buildlCount = 0;
+    float comBuildArea = 0.0;
     house hs;
     building build;
     std::vector<building> blds;
@@ -112,39 +109,30 @@ struct street
 
 void fullRoom (storey &flr, float area)
 {
-    int nm = 0;
-    //while (area > MINAREAROOM)
-    //{
-        for (int m = flr.roomCount; m > 0; m--)
-        {
+   int nm = 0;
+   for (int m = flr.roomCount; m > 0; m--)
+   {
+        std::cout << "Enter name of room from " << LIVINGROOM << " to " << NURSERY << "\n";
+        std::cout << LIVINGROOM << " - Livingroom\n" << BATHROOM << " - Bathhouse\n";
+        std::cout << KITCHEN << " - Kitchen\n" << BEDROOM << " - Bedroom\n" << NURSERY << " - Nursery\n=>";
 
-            std::cout << "Enter name of room from " << LIVINGROOM << " to " << NURSERY << "\n";
-            std::cout << LIVINGROOM << " - Livingroom\n" << BATHROOM << " - Bathhouse\n";
-            std::cout << KITCHEN << " - Kitchen\n" << BEDROOM << " - Bedroom\n" << NURSERY << " - Nursery\n=>";
+        //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+        std::cin >> nm;
+        flr.rm.name = (roomNames)nm;
+        //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-            //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-            std::cin >> nm;
-            flr.rm.name = (roomNames)nm;
-            //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+        std::cout << "Enter room's area from  " << MINAREAROOM;
+        std::cout << " to " << area - MINAREAROOM * (float)(m - 1) << "\n=>";
 
-            //str.sctn.hs.floor.rm.name = (roomNames)(4 - m + 1) ; // если по порядку
+        //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+        std::cin >> flr.rm.area;
+        //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-            std::cout << "Enter room's area from  " << MINAREAROOM;
-            std::cout << " to " << area - MINAREAROOM * (float)(m - 1) << "\n=>";
+        area -= flr.rm.area;
+        std::cout << "Rest area flor is " << area << "\n";
 
-            //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-            std::cin >> flr.rm.area;
-            //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-            //flr.rm.area = 12.0; // площадь не больше остатка площади этажа
-
-            area -= flr.rm.area;
-
-            std::cout << "Rest area flor is " << area << "\n";
-
-            flr.rooms.push_back(flr.rm);
-            //if (area < MINAREAROOM) break;
-        }
-    //}
+        flr.rooms.push_back(flr.rm);
+   }
 }
 
 void fullStorey (house &hs)
@@ -197,6 +185,8 @@ void fullYard (section &sct, float area)
             //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
             //sct.build.area = 20.0; // введите площадь строения (меньше, чем остсток площади, но больше чем 20)
 
+            sct.comBuildArea += sct.build.area;
+
             std::cout << "Enter name of building from " << GARAGE << " to " << BARN << "\n";
             std::cout << GARAGE << " - Garage " << BATHHOUSE << " - Bathhouse " << BARN << "- Barn\n=>";
 
@@ -212,14 +202,8 @@ void fullYard (section &sct, float area)
             area -= sct.build.area;
 
             std::cout << "areaRestSect is " << area << std::endl;
-
             sct.blds.push_back(sct.build);
-
-
-            //if (area < MINAREABUILDING)
-              //  break;
         }
-    //}
 }
 
 void fullStreet (street &str, int n)
@@ -380,10 +364,7 @@ void showSection (std::vector<section> scts, int n)
         std::cout << "\t\tArea: " << scts [i].area << "\n";
         std::cout << "\t\tHOUSE:\n";
         std::cout << "\t\t\tArea: " << scts[i].hs.area << "\n";
-        std::cout << "\t\t\tNumber of floors: " << scts[i].hs.florCount << "\n";
-
-
-
+        std::cout << "\t\t\tNumber of floors: " << scts[i].hs.florCount;
 
         showHouse (scts[i].hs);
         if (!scts[i].buildlCount)
@@ -450,38 +431,31 @@ std::cout << "=======================================================\n";
     //Общая площадь всех участков
     float vilageArea = 0.0;
     float commonHouseArea = 0.0;
-    float buildingsArea = 0.0;
     float commonBuildingsArea = 0.0;
+    float commonBuildingsHousArea = 0.0;
 
     for (int i = 0; i < strts.size(); i++)
     {
         for (int j = 0; j < strts[i].sects.size(); j++)
         {
             commonHouseArea += strts[i].sects[j].hs.area;
+            commonBuildingsArea += strts[i].sects[j].comBuildArea;
             vilageArea += strts[i].sects[j].area;
-            for (int k = 0; k < strts[i].sects[j].blds.size(); k++)
-            {
-                buildingsArea =+ strts[i].sects[j].blds[k].area;
-            }
-
-            commonBuildingsArea += (commonHouseArea + buildingsArea);
-            std::cout << "commonSettArea = " << vilageArea << std::endl;
+            commonBuildingsHousArea += (commonHouseArea + commonBuildingsArea);
         }
     }
-
-
 
 std::cout << "=======================================================\n";
 
 
-    std::cout << "All houses area is = " << commonHouseArea;
-    std::cout << "All buildings is = " << buildingsArea;
-    std::cout << "Common buidings area is = " << commonBuildingsArea;
-
     std::cout << "Vilages area is = " << vilageArea << std::endl;
+    std::cout << "All houses area is = " << commonHouseArea << std::endl;
+    std::cout << "All buildings is = " << commonBuildingsArea << std::endl;
+    std::cout << "Common buidings area is = " << commonBuildingsHousArea << std::endl;
 
-    float perscent = commonBuildingsArea/vilageArea * 100;
-    std::cout << "The share of houses is " << perscent << "% of the area of the entire village";
+
+    float perscent = commonBuildingsHousArea/vilageArea * 100;
+    std::cout << "The share of buildings is " << perscent << "% of the area of the entire village";
 
 
 
